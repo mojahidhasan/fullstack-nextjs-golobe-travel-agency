a93e28fd00838daabe97714eb637deec4b20fa71
mojahidhasan <70759981+mojahidhasan@users.noreply.github.com>
2024-05-19 19:51:29 +0600

    some improvements

--- middleware.js
+++ middleware.js
@@ -12,5 +12,5 @@ export default auth((req) => {
 });
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
 };
