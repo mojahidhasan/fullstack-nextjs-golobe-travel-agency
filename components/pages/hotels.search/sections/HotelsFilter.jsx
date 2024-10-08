43e4dc38eac2f79f6e98d912fdfc2887b3d43d15
mojahidhasan <c.mojahid.me@gmail.com>
2024-10-08 09:11:49 +0600

    hotel filter partially functionaled

--- components/pages/hotels.search/sections/HotelsFilter.jsx
+++ components/pages/hotels.search/sections/HotelsFilter.jsx
@@ -11,52 +11,36 @@ import { cn } from "@/lib/utils";
 import { useState, useEffect } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
-
 import {
-  setFilterAirlines,
-  setFilterRate,
-  setFilterTrips,
-  setFilterPrice,
-  setFilterDepartureTime,
-  resetFilters,
-} from "@/reduxStore/features/flightFormSlice";
-
+  setStayFilter,
+  resetStayFilters,
+} from "@/reduxStore/features/stayFormSlice";
 export function HotelsFilter({ className }) {
   const [filter, setFilter] = useState(false);
 
   const dispatch = useDispatch();
 
-  const handleAirplaneFilter = (event) => {
-    if (event.currentTarget.dataset.state !== "checked") {
-      dispatch(
-        setFilterAirlines({
-          add: event.currentTarget.labels[0].textContent,
-        })
-      );
-    } else {
-      dispatch(
-        setFilterAirlines({
-          remove: event.currentTarget.labels[0].textContent,
-        })
-      );
-    }
-  };
-  const handleTripsFilter = (event) => {
-    if (event.currentTarget.dataset.state !== "checked") {
-      dispatch(
-        setFilterTrips({
-          add: event.currentTarget.labels[0].textContent,
-        })
-      );
-    } else {
-      dispatch(
-        setFilterTrips({
-          remove: event.currentTarget.labels[0].textContent,
-        })
-      );
-    }
-  };
+  const stayFilterState = useSelector(
+    (selector) => selector.stayForm.value.filters
+  );
 
+  function handleCheckedChange(checked, groupName, name) {
+    if (checked) {
+      dispatch(
+        setStayFilter({
+          [groupName]: [...stayFilterState[groupName], name],
+        })
+      );
+    } else {
+      dispatch(
+        setStayFilter({
+          [groupName]: stayFilterState[groupName].filter(
+            (item) => item !== name
+          ),
+        })
+      );
+    }
+  }
   return (
     <section
       className={cn(
@@ -64,7 +48,7 @@ export function HotelsFilter({ className }) {
         className
       )}
     >
-      <div className="flex items-center justify-between mb-[32px] font-semibold text-secondary">
+      <div className="flex items-center justify-between mb-[16px] font-semibold text-secondary">
         <Button
           className="text-[1.25rem] p-0"
           variant={"link"}
@@ -77,97 +61,101 @@ export function HotelsFilter({ className }) {
         >
           <h2>Fliters</h2>
         </Button>
-        <Button variant={"link"} onClick={() => dispatch(resetFilters())}>
+        <Button variant={"link"} onClick={() => dispatch(resetStayFilters())}>
           reset
         </Button>
       </div>
       <div
         className={cn(
-          "w-full max-lg:absolute max-lg:z-50 max-lg:bg-white max-lg:p-5",
+          "w-full max-lg:bg-white max-lg:p-5 drop-shadow-lg rounded-lg",
           filter === false && "max-lg:hidden"
         )}
       >
-        <form onSubmit={(e) => e.preventDefault()}>
+        <div>
           <Dropdown title={"Price"} open>
             <div className="my-5">
               <Slider
                 name="price-slider"
                 min={50}
                 max={1200}
-                defaultValue={[50, 1200]}
+                value={stayFilterState.priceRange}
                 onValueChange={(value) => {
-                  dispatch(setFilterPrice(value));
+                  dispatch(setStayFilter({ priceRange: value }));
                 }}
               />
             </div>
           </Dropdown>
           <Dropdown title={"Rating"} open>
             <FilterRating
-              setRating={(rate) => {
-                dispatch(setFilterRate(rate));
+              value={stayFilterState.rate}
+              setValue={(rate) => {
+                dispatch(setStayFilter({ rate }));
               }}
               className="justify-start"
             />
           </Dropdown>
           <Dropdown title={"Freebies"} open>
             <div className="flex flex-col gap-3">
-              <Checkbox
-                onClick={handleAirplaneFilter}
-                name={"Free-breakfast"}
-                id="Free-breakfast"
-                label="Free breakfast"
-              />
-              <Checkbox
-                onClick={handleAirplaneFilter}
-                name={"Free-parking"}
-                id="Free-parking"
-                label="Free parking"
-              />
-              <Checkbox
-                onClick={handleAirplaneFilter}
-                name={"Free-internet"}
-                id="Free-internet"
-                label="Free internet"
-              />
-              <Checkbox
-                onClick={handleAirplaneFilter}
-                name={"Free-airport-shuttle"}
-                id="Free-airport-shuttle"
-                label="Free airport shuttle"
-              />
-              <Checkbox
-                onClick={handleAirplaneFilter}
-                name={"Free-cancellation"}
-                id="Free-cancellation"
-                label="Free cancellation"
-              />
+              {[
+                "Free breakfast",
+                "Free parking",
+                "Free internet",
+                "Free airport shuttle",
+                "Free cancellation",
+              ].map((name) => {
+                const IDfyName = name.split(" ").join("").toLocaleLowerCase();
+                return (
+                  <Checkbox
+                    key={IDfyName}
+                    onClick={(checked) =>
+                      handleCheckedChange(checked, "freebies", IDfyName)
+                    }
+                    name={IDfyName}
+                    id={IDfyName}
+                    label={name}
+                    checked={stayFilterState.freebies.includes(IDfyName)}
+                  />
+                );
+              })}
             </div>
           </Dropdown>
           <Dropdown title={"Amenities"} open>
             <div className="flex flex-col gap-3">
               <Checkbox
-                onClick={handleTripsFilter}
+                onClick={(checked) =>
+                  handleCheckedChange(checked, "amenities", "24hr-front-desk")
+                }
                 name={"24hr-front-desk"}
                 id="24hr-front-desk"
                 label="24hr front desk"
+                checked={stayFilterState.amenities.includes("24hr-front-desk")}
               />
               <Checkbox
-                onClick={handleTripsFilter}
-                name={"Air-conditioned"}
-                id="Air-conditioned"
+                onClick={(checked) =>
+                  handleCheckedChange(checked, "amenities", "air-conditioned")
+                }
+                name={"air-conditioned"}
+                id="air-conditioned"
                 label="Air-conditioned"
+                checked={stayFilterState.amenities.includes("air-conditioned")}
               />
               <Checkbox
-                onClick={handleTripsFilter}
-                name={"Fitness"}
-                id="Fitness"
+                onClick={(checked) =>
+                  handleCheckedChange(checked, "amenities", "fitness")
+                }
+                name={"fitness"}
+                id="fitness"
                 label="Fitness"
+                checked={stayFilterState.amenities.includes("fitness")}
               />
               <Checkbox
-                onClick={handleTripsFilter}
-                name={"Pool"}
-                id="Pool"
+                onClick={(checked) =>
+                  handleCheckedChange(checked, "amenities", "pool")
+                }
+                name={"pool"}
+                id="pool"
                 label="Pool"
+                checked={stayFilterState.amenities.includes("pool")}
               />
               <Button
                 type={"button"}
@@ -178,14 +166,16 @@ export function HotelsFilter({ className }) {
               </Button>
             </div>
           </Dropdown>
-          <Button
-            type={"submit"}
-            onClick={() => setFilter(!filter)}
-            className={"mt-4 float-end bg-primary"}
-          >
-            Submit
-          </Button>
-        </form>
+          <div className="flex justify-end">
+            <Button
+              type={"submit"}
+              onClick={() => setFilter(!filter)}
+              className={"mt-4 bg-primary"}
+            >
+              Submit
+            </Button>
+          </div>
+        </div>
       </div>
     </section>
   );
--- reduxStore/features/stayFormSlice.js
+++ reduxStore/features/stayFormSlice.js
@@ -1,16 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { addDays } from "date-fns";
+
+const defaultValue = {
+  destination: "",
+  checkIn: new Date().toISOString(),
+  checkOut: addDays(new Date(), 1).toISOString(),
+  rooms: 1,
+  guests: 1,
+  promocode: "",
+  filters: {
+    priceRange: [50, 1200],
+    rate: [],
+    freebies: [],
+    amenities: [],
+  },
+};
+
 const stayFormSlice = createSlice({
   name: "stayForm",
   initialState: {
-    value: {
-      destination: "",
-      checkIn: new Date().toISOString(),
-      checkOut: addDays(new Date(), 1).toISOString(),
-      rooms: 1,
-      guests: 1,
-      promocode: "",
-    },
+    value: defaultValue,
   },
   reducers: {
     setStayForm(state, action) {
@@ -19,8 +28,18 @@ const stayFormSlice = createSlice({
         ...action.payload,
       };
     },
+    setStayFilter(state, action) {
+      state.value.filters = {
+        ...state.value.filters,
+        ...action.payload,
+      };
+    },
+    resetStayFilters(state) {
+      state.value.filters = defaultValue.filters;
+    },
   },
 });
 
-export const { setStayForm } = stayFormSlice.actions;
+export const { setStayForm, setStayFilter, resetStayFilters } =
+  stayFormSlice.actions;
 export default stayFormSlice.reducer;
