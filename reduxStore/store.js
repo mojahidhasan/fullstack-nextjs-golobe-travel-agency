f1f94d4e853fd18537da2fe442a17aa3647902d2
Mojahid <c.mojahid.me@gmail.com>
2025-01-03 18:56:20 +0000

    deleted unnecessary redux slice

--- reduxStore/store.js
+++ reduxStore/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import counterSlice from "@/reduxStore/features/counterSlice";
 import flightFormSlice from "@/reduxStore/features/flightFormSlice";
 import stayFormSlice from "@/reduxStore/features/stayFormSlice";
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
-      counter: counterSlice,
       flightForm: flightFormSlice,
       stayForm: stayFormSlice,
     },
