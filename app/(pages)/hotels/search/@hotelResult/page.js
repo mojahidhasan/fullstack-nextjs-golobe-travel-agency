40bc0dc0f220253c72ce44b4ac7205b0ad630e61
Mojahid <c.mojahid.me@gmail.com>
2025-01-10 18:04:06 +0000

    added: implement advanced hotel filtering based on features, amenities, ratings, and price range

--- app/(pages)/hotels/search/@hotelResult/page.js
+++ app/(pages)/hotels/search/@hotelResult/page.js
@@ -4,7 +4,7 @@ import { auth } from "@/lib/auth";
 import { RATING_SCALE } from "@/lib/constants";
 export default async function HotelResultPage({ searchParams }) {
   let hotels = [];
-
+  let filters = {};
   const session = await auth();
 
   if (Object.keys(searchParams).length > 0) {
@@ -14,16 +14,36 @@ export default async function HotelResultPage({ searchParams }) {
     const guests = searchParams.guests;
     const rooms = searchParams.rooms;
 
-    hotels = await getManyDocs(
-      "Hotel",
-      {
-        query: {
-          $regex: `${destination.match(/.{1,2}/g).join("+?.*")}`,
-          $options: "i",
+    if (searchParams?.filters) {
+      filters = JSON.parse(searchParams.filters);
+      hotels = await getManyDocs(
+        "Hotel",
+        {
+          query: {
+            $regex: `${destination.match(/.{1,2}/g).join("+?.*")}`,
+            $options: "i",
+          },
+          ...((filters?.features && filters?.features.length > 0) ?? {
+            features: { $in: filters.features },
+          }),
+          ...((filters?.amenities && filters?.amenities.length > 0) ?? {
+            amenities: { $in: filters.amenities },
+          }),
         },
-      },
-      ["hotels"]
-    );
+        ["hotels"]
+      );
+    } else {
+      hotels = await getManyDocs(
+        "Hotel",
+        {
+          query: {
+            $regex: `${destination.match(/.{1,2}/g).join("+?.*")}`,
+            $options: "i",
+          },
+        },
+        ["hotels"]
+      );
+    }
 
     // filter by total available rooms sleeps count is greater than or equal to number of guests
     hotels = hotels.filter((hotel) => {
@@ -35,6 +55,7 @@ export default async function HotelResultPage({ searchParams }) {
             new Date(availability.checkedIn) <= new Date(checkOut)
           );
         });
+
         return isRoomAvailable;
       });
 
@@ -45,7 +66,6 @@ export default async function HotelResultPage({ searchParams }) {
 
       return totalSleepsCount >= Number(guests);
     });
-
     // show liked hotels if user is logged in
     if (session?.user?.id) {
       const userDetails = await getOneDoc("User", { _id: session?.user?.id }, [
@@ -60,34 +80,68 @@ export default async function HotelResultPage({ searchParams }) {
   }
 
   // rating and reviews
-  hotels = hotels.map((hotel) => {
-    const totalRatingsSum = hotel.reviews.reduce(
-      (acc, review) => acc + +review.rating,
-      0
-    );
-    const totalReviewsCount = hotel.reviews.length;
+  hotels = hotels
+    .map((hotel) => {
+      const totalRatingsSum = hotel.reviews.reduce(
+        (acc, review) => acc + +review.rating,
+        0
+      );
+      const totalReviewsCount = hotel.reviews.length;
 
-    const rating = totalRatingsSum / totalReviewsCount;
-    const ratingScale = RATING_SCALE[Math.floor(rating)];
+      const rating = totalRatingsSum / totalReviewsCount;
+      const ratingScale = RATING_SCALE[Math.floor(rating)];
 
-    const cheapestRoom = [...hotel.rooms].sort(
-      (a, b) => a.price.base - b.price.base
-    )[0];
-    return {
-      _id: hotel._id,
-      slug: hotel.slug,
-      name: hotel.name,
-      address: Object.values(hotel.address).join(", "),
-      amenities: hotel.amenities.slice(0, 5),
-      price: cheapestRoom.price,
-      availableRooms: hotel.rooms.length,
-      rating: totalReviewsCount ? rating.toFixed(1) : "N/A",
-      totalReviews: totalReviewsCount,
-      ratingScale: ratingScale || "N/A",
-      image: hotel.images[0],
-      liked: hotel.liked,
-    };
-  });
+      const cheapestRoom = [...hotel.rooms].sort((a, b) => {
+        const aPrice =
+          +a.price.base +
+          +a.price.taxe -
+          +a.price.discount +
+          +a.price.serviceFee;
+        const bPrice =
+          +b.price.base +
+          +b.price.taxe -
+          +b.price.discount +
+          +b.price.serviceFee;
+        return aPrice - bPrice;
+      })[0];
+      return {
+        _id: hotel._id,
+        slug: hotel.slug,
+        name: hotel.name,
+        address: Object.values(hotel.address).join(", "),
+        amenities: hotel.amenities.slice(0, 5),
+        price: cheapestRoom.price,
+        availableRooms: hotel.rooms.length,
+        rating: totalReviewsCount ? rating.toFixed(1) : "N/A",
+        totalReviews: totalReviewsCount,
+        ratingScale: ratingScale || "N/A",
+        image: hotel.images[0],
+        liked: hotel.liked,
+      };
+    })
+    .filter((hotel) => {
+      let rateFilter = true;
+      let priceFilter = true;
+      if (filters?.rate && filters?.rate.length > 0) {
+        const rating = hotel.rating === "N/A" ? 0 : +hotel.rating;
+        rateFilter = filters?.rate.map(Number).includes(Math.floor(rating));
+      }
+
+      if (filters?.priceRange?.length > 1) {
+        const price =
+          +hotel.price.base +
+          +hotel.price.tax -
+          +hotel.price.discount +
+          +hotel.price.serviceFee;
+        if (
+          price <= +filters?.priceRange[0] ||
+          price >= +filters?.priceRange[1]
+        ) {
+          priceFilter = false;
+        }
+      }
+      return rateFilter && priceFilter;
+    });
 
   if (hotels?.length < 1) {
     return <div className={"text-center grow font-bold"}>No data found</div>;
