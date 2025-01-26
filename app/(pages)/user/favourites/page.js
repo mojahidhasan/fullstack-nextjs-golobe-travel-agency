dd287422238793cfd9215317636463cddcc927b3
Mojahid <c.mojahid.me@gmail.com>
2025-01-26 20:13:33 +0000

    added: implement fetching and displaying user's favorite hotels with reviews and ratings

--- app/(pages)/user/favourites/page.js
+++ app/(pages)/user/favourites/page.js
@@ -74,11 +74,62 @@ export default async function FavouritesPage() {
 
     favouriteFlights = favouriteFlights.filter(Boolean);
     // will be added later
-    // if (userDetails.likes.hotels.length > 0) {
-    //   favouriteHotels = await getHotelssByHoteIdsCached(
-    //     userDetails.likes.hotels
-    //   );
-    // }
+    if (userDetails.likes.hotels.length > 0) {
+      // eslint-disable-next-line no-undef
+      favouriteHotels = await Promise.all(
+        userDetails.likes.hotels.map(async (hotel) => {
+          const hotelDetails = await getOneDoc(
+            "Hotel",
+            {
+              _id: hotel,
+            },
+            [hotel, "hotels"]
+          );
+          if (Object.keys(hotelDetails).length === 0) return;
+          const hotelReviews = await getManyDocs(
+            "HotelReview",
+            {
+              hotelId: hotelDetails._id,
+            },
+            [hotel._id + "_review", "hotelReviews"]
+          );
+
+          const totalReviewsCount = hotelReviews.length;
+          const rating = hotelReviews.reduce((acc, review) => {
+            return +acc + +review.rating;
+          }, 0);
+          const ratingScale =
+            RATING_SCALE[Math.floor(rating / totalReviewsCount)];
+          const cheapestRoom = [...hotelDetails.rooms].sort((a, b) => {
+            const aPrice =
+              +a.price.base +
+              +a.price.taxe -
+              +a.price.discount +
+              +a.price.serviceFee;
+            const bPrice =
+              +b.price.base +
+              +b.price.taxe -
+              +b.price.discount +
+              +b.price.serviceFee;
+            return aPrice - bPrice;
+          })[0];
+          return {
+            _id: hotelDetails._id,
+            slug: hotelDetails.slug,
+            name: hotelDetails.name,
+            address: Object.values(hotelDetails.address).join(", "),
+            amenities: hotelDetails.amenities.slice(0, 5),
+            price: cheapestRoom.price,
+            availableRooms: hotelDetails.rooms.length,
+            rating: totalReviewsCount ? rating.toFixed(1) : "N/A",
+            totalReviews: totalReviewsCount,
+            ratingScale: ratingScale || "N/A",
+            image: hotelDetails.images[0],
+            liked: true,
+          };
+        })
+      );
+    }
 
     return (
       <main className={"mx-auto mb-[90px] w-[95%] sm:w-[90%]"}>
