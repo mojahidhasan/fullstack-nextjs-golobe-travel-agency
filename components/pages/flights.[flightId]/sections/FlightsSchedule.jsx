5b03f815c037f59b6ef0f7dafbcbe394c3042eb0
Mojahid <c.mojahid.me@gmail,com>
2025-03-17 12:58:32 +0600

    refactor: enhance FlightsSchedule component to handle flight segments and layover display

--- components/pages/flights.[flightId]/sections/FlightsSchedule.jsx
+++ components/pages/flights.[flightId]/sections/FlightsSchedule.jsx
@@ -1,22 +1,64 @@
 import { FlightScheduleCard } from "@/components/FlightScheduleCard";
-import { minutesToHMFormat, substractTimeInMins } from "@/lib/utils";
-export function FlightsSchedule({ flight }) {
+import { minutesToHMFormat } from "@/lib/utils";
+export function FlightsSchedule({ flight, metaData }) {
+  let flightSegments = [];
 
+  let currentDeparture = flight.departure,
+    currentArrival = flight.arrival,
+    currentDepartureAirline = flight.airlineId,
+    duration = flight.totalDuration,
+    airplane = flight.airplaneId;
+
+  if (flight.stopovers.length > 0) {
+    for (let i = 0; i <= flight.stopovers.length; i++) {
+      if (i < flight.stopovers.length) {
+        currentArrival = flight.stopovers[i].arrival;
+        duration = flight.stopovers[i].duration.arrivalFromOrigin;
+      }
+
+      flightSegments.push({
+        departure: currentDeparture,
+        arrival: currentArrival,
+        duration,
+        airline: currentDepartureAirline,
+        airplane,
+        layover: flight.stopovers[i]?.layover,
+      });
+
+      if (i < flight.stopovers.length) {
+        currentDeparture = {
+          ...currentArrival,
+          scheduled: +flight.stopovers[i].departure.scheduled,
+        };
+        duration = flight.stopovers[i].duration.arrivalToDestination;
+        airplane = flight.stopovers[i].airplaneId;
+      }
+
+      if (i === flight.stopovers.length - 1) {
+        currentArrival = flight.arrival;
+      }
+    }
+  } else {
+    flightSegments.push({
+      departure: currentDeparture,
+      arrival: currentArrival,
+      duration,
+      airline: currentDepartureAirline,
+      airplane,
+    });
+  }
   return (
     <section className="mb-[120px] p-0 text-secondary flex flex-col gap-3">
       {
-        flight.stopovers.map((stopover, index) => {
-          const order = {
-            0: "order-1",
-            1: "order-3"
-          };
-          return <FlightScheduleCard className={ order[index] } key={ index } flightScheduleDetails={ { ...stopover, price: flight.price, timezone: flight.timezone } } />;
+        flightSegments.map((stopover, index) => {
+          return <>
+            <FlightScheduleCard key={ index } flightScheduleDetails={ { ...stopover, price: flight.price, timezone: metaData.timezone } } />
+            { stopover.layover && <div className={ "text-center bg-tertiary rounded-md text-white font-bold w-fit px-5 py-1 self-center" }>
+              Layover { minutesToHMFormat(stopover.layover) }
+            </div> }
+          </>;
         }
         )
-      }{
-        (flight.stopovers.length > 1) && <div className={ "order-2 text-center bg-tertiary rounded-md text-white font-bold w-fit px-5 py-1 self-center" }>
-          Layover { minutesToHMFormat(substractTimeInMins(flight.stopovers[1].departureDateTime, flight.stopovers[0].arrivalDateTime)) }
-        </div>
       }
     </section>
   );
