8b781519822cba787773a7d3c23ab7a88d7cd8b6
Mojahid <c.mojahid.me@gmail,com>
2025-03-17 12:55:43 +0600

    refactor: update FlightScheduleCard as some flight props of db have been changed

--- components/FlightScheduleCard.jsx
+++ components/FlightScheduleCard.jsx
@@ -1,6 +1,6 @@
 import { Separator } from "@/components/ui/separator";
 import Image from "next/image";
-import { airlines } from "@/data/airlinesLogos";
+import airlinesLogos from "@/data/airlinesLogos";
 import plane from "@/public/icons/airplane-filled.svg";
 import wifi from "@/public/icons/wifi.svg";
 import stopwatch from "@/public/icons/stopwatch.svg";
@@ -13,34 +13,32 @@ import { cn } from "@/lib/utils";
 import { formatInTimeZone } from "date-fns-tz";
 export function FlightScheduleCard({ flightScheduleDetails, variant = "default", className }) {
   const {
-    departureDate,
+    departure,
+    arrival,
     duration,
-    departureDateTime,
-    departureAirport,
-    arrivalDateTime,
-    arrivalAirport,
-    airplaneName,
     airline,
+    airplane,
     price,
-  } = processFlightScheduleDetails(flightScheduleDetails);
+    timezone
+  } = flightScheduleDetails;
   return (
     <div className={ cn(" shadow-lg rounded-[12px] bg-white px-[24px] py-[32px] shadow-small", className) }>
       { variant === "book" && (
         <div className="mb-[24px] flex justify-between font-bold">
-          <h3 className="font-tradeGothic text-[1.5rem]">{ airplaneName }</h3>
-          <p className="text-[2rem] text-tertiary">${ price }</p>
+          <h3 className="font-tradeGothic text-[1.5rem]">{ airplane.model }</h3>
+          <p className="text-[2rem] text-tertiary">${ price.total }</p>
         </div>
       ) }
       <div className="mb-[24px] flex justify-between font-bold">
-        <h4 className="font-tradeGothic text-[1.25rem]">Depart { departureDate }</h4>
-        <p className="font-medium opacity-75">{ duration }</p>
+        <h4 className="font-tradeGothic text-[1.25rem]">Depart { formatInTimeZone(+departure.scheduled, timezone, "eee, MMM d") }</h4>
+        <p className="font-medium opacity-75">{ minutesToHMFormat(+duration) }</p>
       </div>
       <div className="mb-[40px] grid justify-between gap-[20px] md:flex">
         <div className="flex w-fit items-center gap-[24px] rounded-[8px] border border-primary px-[32px] py-[16px]">
           <Image
             height={ 40 }
             width={ 60 }
-            src={ airlines[airline.iataCode] }
+            src={ airlinesLogos[airline.iataCode] }
             alt="emirates_logo"
             className="h-[40px] w-auto"
           />
@@ -49,7 +47,7 @@ export function FlightScheduleCard({ flightScheduleDetails, variant = "default",
               { airline.name }
             </h3>
             <p className="text-[0.875rem] font-medium opacity-60">
-              { airplaneName }
+              { airplane.model }
             </p>
           </div>
         </div>
@@ -73,9 +71,9 @@ export function FlightScheduleCard({ flightScheduleDetails, variant = "default",
       <div className="grid grid-rows-5 items-center gap-[40px] md:flex md:justify-center lg:gap-[80px]">
         <div>
           <span className="text-[1rem] font-semibold lg:text-[1.5rem]">
-            { departureDateTime }
+            { formatInTimeZone(+departure.scheduled, timezone, "h:mm aaa") }
           </span>{ " " }
-          <span className="max-lg:text-[0.75rem]">{ departureAirport }</span>
+          <span className="max-lg:text-[0.75rem]">{ `${departure.airport.name}(${departure.airport.iataCode})` }</span>
         </div>
         <div className="row-span-3 flex h-max w-max items-center gap-[32px] max-md:rotate-90 md:gap-[24px]">
           <Image
@@ -101,33 +99,10 @@ export function FlightScheduleCard({ flightScheduleDetails, variant = "default",
           />
         </div>
         <div>
-          <span className="font-semibold lg:text-[1.5rem]">{ arrivalDateTime }</span>{ " " }
-          <span className="max-lg:text-[0.75rem]">{ arrivalAirport }</span>
+          <span className="font-semibold lg:text-[1.5rem]">{ formatInTimeZone(+arrival.scheduled, timezone, "h:mm aaa") }</span>{ " " }
+          <span className="max-lg:text-[0.75rem]">{ `${arrival.airport.name}(${arrival.airport.iataCode})` }</span>
         </div>
       </div>
     </div>
   );
-}
-function processFlightScheduleDetails(data) {
-  return {
-    departureDate: formatInTimeZone(new Date(data.departureDateTime), data.timezone, "eee, MMM d"),
-    duration: minutesToHMFormat(
-      data.duration
-    ),
-    departureDateTime: formatInTimeZone(new Date(data.departureDateTime), data.timezone, "h:mm aaa"),
-    departureAirport:
-      data.departureAirportId.name.split(",")[0] +
-      "(" +
-      data.departureAirportId.iataCode +
-      ")",
-    arrivalDateTime: formatInTimeZone(new Date(data.arrivalDateTime), data.timezone, "h:mm aaa"),
-    arrivalAirport:
-      data.arrivalAirportId.name.split(",")[0] +
-      "(" +
-      data.arrivalAirportId.iataCode +
-      ")",
-    airline: data.airlineId,
-    airplaneName: data.airplaneId.model,
-    price: data.price,
-  };
 }
