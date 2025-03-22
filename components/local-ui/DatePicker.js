98a80c962b990df0d71c9ef616ddbbf9939e5ce1
Mojahid <c.mojahid.me@gmail,com>
2025-03-22 16:09:47 +0600

    add: enhanced version of DatePicker component for selecting and displaying dates with calendar functionality

---
+++ components/local-ui/DatePicker.js
@@ -0,0 +1,92 @@
+"use client";
+import * as React from "react";
+import { format } from "date-fns";
+
+import { cn, isDateObjValid } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+export function DatePicker({
+  className,
+  getDate = () => {},
+  date,
+  disabledDates = [],
+  ...props
+}) {
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+  const { useState, useEffect } = React;
+
+  const [value, setValue] = useState(date);
+  const [dateState, setDateState] = useState(date);
+  const [month, setMonth] = useState(date);
+
+  useEffect(() => {
+    getDate(dateState);
+    setValue(dateState);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dateState?.toString()]);
+
+  useEffect(() => {
+    setValue(date);
+    setDateState(date);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [date?.toString()]);
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        {isDateObjValid(value) ? (
+          <div className={"w-full h-full p-4"}>
+            <div className={"text-xl font-bold"}>
+              {format(value, "dd MMM yy")}
+            </div>
+            <div className={"text-md font-medium"}>{format(value, "EEEE")}</div>
+          </div>
+        ) : (
+          <div className={"w-full h-full p-4"}>
+            <div className={"text-xl font-bold"}>DD MMM YY</div>
+            <div className={"text-md font-medium"}>Weekday</div>
+          </div>
+        )}
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          timeZone={timeZone}
+          showOutsideDays={true}
+          disabled={disabledDates}
+          className={"bg-primary/60"}
+          classNames={{
+            day_today: "border border-secondary",
+          }}
+          {...(isDateObjValid(date) && {
+            selected: date,
+            month: new Date(),
+          })}
+          onSelect={setDateState}
+          onMonthChange={setMonth}
+          initialFocus
+          animate
+          required
+          {...props}
+        />
+        <Button
+          className="w-full rounded-none"
+          onClick={() => {
+            const now = new Date();
+            setMonth(now);
+            setDateState(now);
+          }}
+        >
+          Today
+        </Button>
+      </PopoverContent>
+    </Popover>
+  );
+}
