56e09ea0c93219f52901e415117ab09d0769d615
Mojahid <c.mojahid.me@gmail,com>
2025-03-22 17:54:56 +0600

    fix: count was not getting saved state from redux store. count was resetting each new rerender. so update Counter component to correctly manage and display count state ignoring infinite loop.

--- components/local-ui/Counter.jsx
+++ components/local-ui/Counter.jsx
@@ -3,34 +3,32 @@ import { useState, useEffect } from "react";
 import { PlusCircle, MinusCircle } from "lucide-react";
 import { cn } from "@/lib/utils";
 export default function Counter({ className, defaultCount = 0, maxCount = Infinity, minCount = -Infinity, getCount = () => { } }) {
-  const [value, setValue] = useState(0);
-  const [count, setCount] = useState(0);
+
+  const [count, setCount] = useState(() => defaultCount);
 
   useEffect(() => {
-    getCount(count);
-    setValue(count);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [count]);
-
-  useEffect(() => {
-    setValue(defaultCount);
+    setCount(defaultCount);
   }, [defaultCount]);
 
+
   function handleIncrement() {
-    if (count >= maxCount) {
-      return;
-    }
-    setCount((count) => count + 1);
+    setCount((prev) => {
+      const newCount = prev < maxCount ? prev + 1 : prev;
+      getCount(newCount);
+      return newCount;
+    });
   }
 
   function handleDecrement() {
-    if (count > minCount) {
-      setCount((count) => count - 1);
-    }
+    setCount((prev) => {
+      const newCount = prev > minCount ? prev - 1 : prev;
+      getCount(newCount);
+      return newCount;
+    });
   }
   return <div className={ cn("flex w-fit items-center gap-2 h-8", className) }>
     <button className={ "w-fit h-full" } onClick={ handleDecrement }><MinusCircle width={ 24 } height={ 24 } className={ "w-full h-full text-primary" } /></button>
-    <p className={ "h-fit w-8 text-center text-md font-bold" }>{ value }</p>
+    <p className={ "h-fit w-8 text-center text-md font-bold" }>{ count }</p>
     <button className={ "w-fit h-full" } onClick={ handleIncrement }><PlusCircle width={ 24 } height={ 24 } className={ "w-full h-full text-primary" } /></button>
   </div>;
 
