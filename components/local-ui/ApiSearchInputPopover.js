40741d65b2db36712b0c852f535fb1adce4ae73c
Mojahid <c.mojahid.me@gmail,com>
2025-03-22 16:06:01 +0600

    added: ApiSearchInputPopover component for enhanced search flight or hotel to api and render the result

---
+++ components/local-ui/ApiSearchInputPopover.js
@@ -0,0 +1,135 @@
+"use client";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import Image from "next/image";
+import { cn, debounce } from "@/lib/utils";
+
+import { useState, useEffect, useRef } from "react";
+import { Skeleton } from "../ui/skeleton";
+
+import searchIcon from "@/public/icons/search.svg";
+export function ApiSearchInputPopover({
+  className,
+  defaultSelected = {},
+  fetchInputs,
+  getSelectedResult = () => {},
+  renderSelectedResult = () => {},
+  getSearchResults = () => {},
+  renderSearchResults = () => {},
+}) {
+  const [open, setOpen] = useState(false);
+  const [val, setVal] = useState(defaultSelected);
+  const [selectedState, setSelectedState] = useState(defaultSelected);
+
+  useEffect(() => {
+    getSelectedResult(selectedState);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [JSON.stringify(selectedState)]);
+
+  useEffect(() => {
+    setVal(defaultSelected);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [JSON.stringify(defaultSelected)]);
+  return (
+    <>
+      <Popover className="relative" open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild className={cn("cursor-pointer", className)}>
+          {renderSelectedResult(val)}
+        </PopoverTrigger>
+        <PopoverContent className="w-[300px] md:w-[400px] p-2" align="center">
+          <SearchResults
+            setOpen={setOpen}
+            setSelected={setSelectedState}
+            getSelectedResult={getSelectedResult}
+            getSearchResults={getSearchResults}
+            renderSearchResults={renderSearchResults}
+            fetchInputs={fetchInputs}
+          />
+        </PopoverContent>
+      </Popover>
+    </>
+  );
+}
+
+function SearchResults({
+  setOpen = () => {},
+  setSelected = () => {},
+  getSearchResults = () => {},
+  renderSearchResults = () => {},
+  fetchInputs = {
+    url: process.env.NEXT_PUBLIC_BASE_URL,
+    method: "GET",
+    searchParamsName: "searchQuery",
+  },
+}) {
+  const [searchQuery, setSearchQuery] = useState("");
+  const urlSearchParam = new URLSearchParams({
+    [fetchInputs.searchParamsName]: searchQuery,
+  }).toString();
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const inputRef = useRef(null);
+  useEffect(() => {
+    async function getData() {
+      setLoading(true);
+      const res = await fetch(`${fetchInputs.url}?${urlSearchParam}`, {
+        method: fetchInputs.method,
+      });
+      if (!res.ok) {
+        return;
+      }
+      const data = await res.json();
+      setData(data);
+      getSearchResults(data);
+      setLoading(false);
+    }
+    getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [urlSearchParam]);
+  const handleInputChange = (e) => {
+    const value = e.target.value || "";
+    setSearchQuery(value);
+  };
+  return (
+    <div>
+      <div className={"flex mb-2"}>
+        <div
+          className={
+            "p-1 flex justify-center items-center border border-r-0 rounded-l rounded-md"
+          }
+        >
+          <Image src={searchIcon} alt={"search_icon"} />
+        </div>
+        <Input
+          ref={inputRef}
+          defaultValue={searchQuery}
+          className="w-full !h-full bg-transparent outline-none hover:bg-muted transition-all border-l-0 rounded-l-[0px]"
+          placeholder="Search..."
+          onChange={debounce(handleInputChange)}
+        />
+      </div>
+      {loading ? (
+        <div className="h-80 flex flex-col gap-2">
+          {[1, 2, 3, 4, 5].map((el) => (
+            <Skeleton key={el} className="h-[60px] w-full" />
+          ))}
+        </div>
+      ) : (
+        <div className="h-80 flex flex-col gap-2 overflow-auto golobe-scrollbar">
+          {data.length < 1 ? (
+            <div className="p-2 font-bold h-full flex items-center justify-center text-center text-sm">
+              No results found
+            </div>
+          ) : (
+            renderSearchResults(data, setOpen, setSelected)
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
