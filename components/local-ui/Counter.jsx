64d959d32139eaaaed5ae0d500e0771dac23236b
Mojahid <c.mojahid.me@gmail,com>
2025-03-23 05:42:58 +0600

    refactor: simplify increment and decrement logic in Counter component

--- components/local-ui/Counter.jsx
+++ components/local-ui/Counter.jsx
@@ -10,21 +10,17 @@ export default function Counter({ className, defaultCount = 0, maxCount = Infini
     setCount(defaultCount);
   }, [defaultCount]);
 
+  useEffect(() => {
+    getCount(count);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [count]);
 
   function handleIncrement() {
-    setCount((prev) => {
-      const newCount = prev < maxCount ? prev + 1 : prev;
-      getCount(newCount);
-      return newCount;
-    });
+    setCount((prev) => (prev < maxCount ? prev + 1 : prev));
   }
 
   function handleDecrement() {
-    setCount((prev) => {
-      const newCount = prev > minCount ? prev - 1 : prev;
-      getCount(newCount);
-      return newCount;
-    });
+    setCount((prev) => (prev > minCount ? prev - 1 : prev));
   }
   return <div className={ cn("flex w-fit items-center gap-2 h-8", className) }>
     <button className={ "w-fit h-full" } onClick={ handleDecrement }><MinusCircle width={ 24 } height={ 24 } className={ "w-full h-full text-primary" } /></button>
