4f6838499f818d9bacaae77ded1df117491e91d8
Mojahid <c.mojahid.me@gmail,com>
2025-03-23 06:49:23 +0600

    refactor: improve user name construction for clarity in GET user endpoint

--- app/api/user/[id]/route.js
+++ app/api/user/[id]/route.js
@@ -2,7 +2,9 @@ import { getOneDoc } from "@/lib/db/getOperationDB";
 export async function GET(req, { params }) {
   const getUser = await getOneDoc("User", { _id: params.id }, ["userDetails"]);
   const user = {
-    name: Object.keys(getUser).length ? getUser?.firstname + " " + getUser?.lastname : "A golobe user",
+    name: Object.keys(getUser).length
+      ? getUser?.firstname + " " + getUser?.lastname
+      : "A golobe user",
     profileImage: getUser?.profileImage ?? null,
   };
 
