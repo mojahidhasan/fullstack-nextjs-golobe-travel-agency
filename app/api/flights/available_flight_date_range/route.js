9b4b1206e53b17d8e56155755b30f5ad86fcf0e6
Mojahid <c.mojahid.me@gmail,com>
2025-03-25 02:17:16 +0600

    refactor: replace direct flight date retrieval with getFlightDateRange function

--- app/api/flights/available_flight_date_range/route.js
+++ app/api/flights/available_flight_date_range/route.js
@@ -1,19 +1,7 @@
-import { Flight } from "@/lib/db/models";
-import { addYears } from "date-fns";
-
+import { getFlightDateRange } from "@/lib/controllers/flights";
 export async function GET(req) {
-  const firstFlight = await Flight.findOne({
-    expireAt: { $gte: new Date() },
-  }).sort({ "departure.scheduled": 1 });
-  const lastFlight = await Flight.findOne({}).sort({
-    "departure.scheduled": -1,
-  });
-  const today = new Date().getTime();
-  return Response.json({
-    success: true,
-    data: {
-      from: +firstFlight.departure.scheduled || today,
-      to: +lastFlight.departure.scheduled || addYears(today, 1).getTime(),
-    },
-  });
+  const flightDateRange = await getFlightDateRange();
+  return Response.json(flightDateRange);
 }
+
+export function POST() {}
