5e4df05d3ce7f3f27f7789405b332ed996a3ccd2
Mojahid <c.mojahid.me@gmail,com>
2025-03-25 14:10:31 +0600

    refactor: simplify SetFlightFormState component by removing unused selector and console log

--- components/helpers/setFlightFromState.js
+++ components/helpers/setFlightFromState.js
@@ -1,12 +1,10 @@
 "use client";
 
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setFlightForm } from "@/reduxStore/features/flightFormSlice";
 export function SetFlightFormState({ obj }) {
   const dispatch = useDispatch();
-  const d = useSelector((state) => state.flightForm.value);
-  console.log(d.errors);
   useEffect(() => {
     if (Object.keys(obj).length > 0) dispatch(setFlightForm({ ...obj }));
   }, [JSON.stringify(obj)]);
