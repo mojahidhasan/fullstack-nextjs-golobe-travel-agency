50d4d9b0b6da78b1b94e7c6ea893c84aef19dd5c
Mojahid <c.mojahid.me@gmail.com>
2025-04-26 23:11:18 +0600

    feat: add fade-in and fade-out animations to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -90,10 +90,22 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "fade-in": {
+          from: { visibility: "visible", opacity: 0 },
+          to: { opacity: 1 },
+        },
+        "fade-out": {
+          from: {
+            opacity: 1,
+          },
+          to: { opacity: 0, visibility: "hidden" },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "fade-in": "fade-in 0.2s ease",
+        "fade-out": "fade-out 0.2s ease",
       },
     },
   },
