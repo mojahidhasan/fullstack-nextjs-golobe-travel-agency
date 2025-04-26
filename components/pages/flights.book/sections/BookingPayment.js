1c194a0019637c1aadb6a18d72356c80c7b47f97
Mojahid <c.mojahid.me@gmail.com>
2025-04-27 01:58:25 +0600

    feat: add BookingPayment component for handling payment selection

---
+++ components/pages/flights.book/sections/BookingPayment.js
@@ -0,0 +1,5 @@
+import { ChoosePaymentCard } from "@/components/ChoosePaymentCard";
+// incomplete, working on it currently
+export default function BookingPayment() {
+  return <ChoosePaymentCard />;
+}
