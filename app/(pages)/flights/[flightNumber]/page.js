26f66eaba0091781595949e3a17fc20c488b6938
Mojahid <c.mojahid.me@gmail.com>
2025-04-27 01:30:39 +0600

    feat: refactor FlightDetailsPage to improve session handling and integrate flight details display

--- app/(pages)/flights/[flightNumber]/page.js
+++ app/(pages)/flights/[flightNumber]/page.js
@@ -1,107 +1,99 @@
 import { BreadcrumbUI } from "@/components/local-ui/breadcrumb";
 import { FlightData } from "@/components/pages/flights.[flightId]/sections/FlightData";
 import { EconomyFeatures } from "@/components/pages/flights.[flightId]/sections/EconomyFeatures";
-import { FlightsSchedule } from "@/components/pages/flights.[flightId]/sections/FlightsSchedule";
-import { FlightOrHotelReview } from "@/components/sections/FlightOrHotelReview";
-import { getManyDocs, getOneDoc } from "@/lib/db/getOperationDB";
-import Image from "next/image";
-
-import stopwatch from "@/public/icons/stopwatch.svg";
+import { FlightDetails } from "@/components/pages/flights.[flightId]/sections/FlightsSchedule";
+import { getManyDocs } from "@/lib/db/getOperationDB";
 import { cookies } from "next/headers";
 import { notFound } from "next/navigation";
-import { extractFlightPriceFromAirline } from "@/lib/helpers/flights/priceCalculations";
 import { getUserDetails } from "@/lib/controllers/user";
-import { flightRatingCalculation } from "@/lib/helpers/flights/flightRatingCalculation";
-import { objDeepCompare } from "@/lib/utils";
+import { objDeepCompare, parseFlightSearchParams } from "@/lib/utils";
+import SessionTimeoutCountdown from "@/components/local-ui/SessionTimeoutCountdown";
+import { getFlight } from "@/lib/controllers/flights";
+import dynamic from "next/dynamic";
+import FlightOrHotelReviewsSectionSkeleton from "@/components/local-ui/skeleton/FlightOrHotelReviewsSectionSkeleton";
+import { FareCard } from "@/components/FareCard";
+import { isLoggedIn } from "@/lib/auth";
 
 export default async function FlightDetailsPage({ params }) {
-  const flight = await getOneDoc(
-    "Flight",
-    { flightNumber: params.flightNumber },
-    [params.flightNumber]
+  const loggedIn = await isLoggedIn();
+  const timeZone = cookies().get("timeZone")?.value || "UTC";
+  const metaData = { timeZone, flightClass, isBookmarked: false };
+  const searchState = cookies().get("searchState")?.value || "{}";
+  const parsedSearchState = parseFlightSearchParams(searchState);
+  const flightClass = parsedSearchState?.class || "economy";
+  const alirlinePrices = await getManyDocs("AirlineFlightPrice", {}, [
+    "airlinePrices",
+  ]);
+  const flight = await getFlight(
+    {
+      flightNumber: params.flightNumber,
+      flightClass: flightClass,
+      passengersObj: parsedSearchState?.passengers,
+    },
+    alirlinePrices,
   );
 
   if (Object.keys(flight).length === 0) {
     notFound();
   }
 
-  const userDetails = await getUserDetails();
-  const timezone = cookies().get("timezone")?.value || "UTC";
-  const flightClass = userDetails.flights.latestFlightSearchState.class;
-
-  const metaData = { timezone, flightClass, isBookmarked: false };
-
-  const flightReviews = await getManyDocs(
-    "FlightReview",
-    {
-      airlineId: flight.airlineId,
-      departureAirportId: flight.departure.airport,
-      arrivalAirportId: flight.arrival.airport,
-      airplaneModelName: flight.airplaneId.model,
-    },
-    [params.flightNumber + "_review", flight._id + "_review", "flightReviews"]
-  );
-
-  const cachedAirlines = userDetails.flights?.airlines || [];
-
-  const airlineId = {};
-  const flightAirline = cachedAirlines.find(
-    (el) => el.iataCode === flight.airlineId
-  );
-  airlineId._id = flightAirline._id;
-  airlineId.iataCode = flightAirline.iataCode;
-  airlineId.name = flightAirline.name;
-  airlineId.logo = flightAirline.logo;
-  airlineId.contact = flightAirline.contact;
-
-  flight.airlineId = airlineId;
-  flight.price = extractFlightPriceFromAirline(
-    flight.departure.airport.iataCode,
-    flight.arrival.airport.iataCode,
-    flight.airlineId,
-    flight.priceReductionMultiplierPecentage,
-    flightClass,
-    cachedAirlines
-  );
-  flight.ratingReviews = {
-    rating: flightRatingCalculation(flightReviews),
-    totalReviews: flightReviews.length,
-  };
-
-  metaData.isBookmarked = userDetails.flights.bookmarked.some((el) => {
-    return objDeepCompare(el, {
-      flightId: flight._id,
-      flightNumber: flight.flightNumber,
-      flightClass: metaData.flightClass,
+  if (loggedIn) {
+    const userDetails = await getUserDetails(0);
+    metaData.isBookmarked = userDetails.flights.bookmarked.some((el) => {
+      return objDeepCompare(el, {
+        flightId: flight._id,
+        flightNumber: flight.flightNumber,
+        flightClass: metaData.flightClass,
+      });
     });
-  });
-
+  }
+  const FlightOrHotelReview = dynamic(
+    () => import("@/components/sections/FlightOrHotelReview"),
+    {
+      ssr: false,
+      loading: () => <FlightOrHotelReviewsSectionSkeleton />,
+    },
+  );
   return (
     <>
-      <main className="mx-auto mt-[40px] mb-20 w-[90%]">
+      <main className="mx-auto mb-20 mt-[40px] w-[90%]">
         <div className="my-[40px] w-full">
           <BreadcrumbUI />
         </div>
-        <FlightData data={flight} metaData={metaData} />
-        {/* <EconomyFeatures /> */}
-        {/* <div className="mb-[40px] rounded-[8px] bg-primary/60 p-[16px]">
-          <h3 className="mb-[16px] font-tradeGothic text-[1.5rem] font-bold">
-            Emirates Airlines Policies
-          </h3>
-          <div className="flex gap-[16px] font-medium leading-5">
+        <SessionTimeoutCountdown
+          redirectionLink="/flights"
+          className={"mb-2 rounded-md shadow-lg"}
+        />
+        <div className="flex flex-col gap-3">
+          <FlightData className={"w-full"} data={flight} metaData={metaData} />
+          <FlightDetails
+            className={"w-full"}
+            flight={flight}
+            metaData={metaData}
+          />
+          <div className="w-full rounded-md bg-white p-6 shadow-lg">
+            <h3 className="mb-3 text-xl font-bold">Fare Details</h3>
+            <FareCard className="shadow-none" fare={flight.price} />
+          </div>
+          {/* <EconomyFeatures />
+        <div className="mb-[40px] rounded-[8px] bg-primary/60 p-[16px]">
+        <h3 className="mb-[16px] font-tradeGothic text-[1.5rem] font-bold">
+        Emirates Airlines Policies
+        </h3>
+        <div className="flex gap-[16px] font-medium leading-5">
             <div className="flex grow items-start gap-[8px]">
-              <Image
-                src={stopwatch}
-                height={20}
-                width={20}
-                alt="stopwatch_icon"
-              />
+            <Image
+            src={stopwatch}
+            height={20}
+            width={20}
+            alt="stopwatch_icon"
+            />
               <p className="opacity-75">
-                Pre-flight cleaning, installation of cabin HEPA filters.
+              Pre-flight cleaning, installation of cabin HEPA filters.
               </p>
             </div>
             <div className="flex grow items-start gap-[8px]">
-              <Image
+            <Image
                 src={stopwatch}
                 height={20}
                 width={20}
@@ -112,20 +104,17 @@ export default async function FlightDetailsPage({ params }) {
               </p>
             </div>
           </div>
-        </div> */}
-        <FlightsSchedule flight={flight} metaData={metaData} />
-        <FlightOrHotelReview
-          rating={flight.ratingReviews.rating}
-          reviews={flightReviews}
-          flightOrHotel="flights"
-          reviewKeys={{
-            flightNumber: flight.flightNumber,
-            airlineId: flight.airlineId._id,
-            departureAirportId: flight.departure.airport._id,
-            arrivalAirportId: flight.arrival.airport._id,
-            airplaneModelName: flight.airplaneId.model,
-          }}
-        />
+          </div> */}
+
+          <FlightOrHotelReview
+            className={"w-full"}
+            airlineId={flight.airlineId._id}
+            departureAirportId={flight.departure.airport._id}
+            arrivalAirportId={flight.arrival.airport._id}
+            airplaneModelName={flight.airplaneId.model}
+            flightNumber={flight.flightNumber}
+          />
+        </div>
       </main>
     </>
   );
