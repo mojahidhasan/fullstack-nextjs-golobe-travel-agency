17ba422a74eacb76b16040bcfc2b9bd0beb40861
Mojahid <c.mojahid.me@gmail.com>
2025-04-27 00:08:20 +0600

    feat: refactor FlightScheduleCard to FlightDetailsCard with enhanced layout and baggage details dropdown

--- components/FlightScheduleCard.jsx
+++ components/FlightScheduleCard.jsx
@@ -2,107 +2,142 @@ import { Separator } from "@/components/ui/separator";
 import Image from "next/image";
 import airlinesLogos from "@/data/airlinesLogos";
 import plane from "@/public/icons/airplane-filled.svg";
-import wifi from "@/public/icons/wifi.svg";
-import stopwatch from "@/public/icons/stopwatch.svg";
-import fastFood from "@/public/icons/fast-food.svg";
-import airLineSeat from "@/public/icons/airline-seat.svg";
 import lineLeft from "@/public/icons/line-left.svg";
 import lineRight from "@/public/icons/line-right.svg";
 import { minutesToHMFormat } from "@/lib/utils";
 import { cn } from "@/lib/utils";
 import { formatInTimeZone } from "date-fns-tz";
-export function FlightScheduleCard({ flightScheduleDetails, variant = "default", className }) {
-  const {
-    departure,
-    arrival,
-    duration,
-    airline,
-    airplane,
-    price,
-    timezone
-  } = flightScheduleDetails;
+import { Dropdown } from "./local-ui/Dropdown";
+export function FlightDetailsCard({
+  flight,
+  flightScheduleDetails,
+  className,
+}) {
+  const { departure, arrival, duration, airline, airplane, timeZone } =
+    flightScheduleDetails;
+  const totalAvailableSeats = flight.seatAvailability.reduce((acc, seat) => {
+    if (seat.isAvailable) return acc + 1;
+    return acc;
+  }, 0);
   return (
-    <div className={ cn(" shadow-lg rounded-[12px] bg-white px-[24px] py-[32px] shadow-small", className) }>
-      { variant === "book" && (
-        <div className="mb-[24px] flex justify-between font-bold">
-          <h3 className="font-tradeGothic text-[1.5rem]">{ airplane.model }</h3>
-          <p className="text-[2rem] text-tertiary">${ price.total }</p>
-        </div>
-      ) }
+    <div
+      className={cn(
+        "rounded-[12px] bg-white px-[24px] py-[32px] shadow-lg",
+        className,
+      )}
+    >
       <div className="mb-[24px] flex justify-between font-bold">
-        <h4 className="font-tradeGothic text-[1.25rem]">Depart { formatInTimeZone(+departure.scheduled, timezone, "eee, MMM d") }</h4>
-        <p className="font-medium opacity-75">{ minutesToHMFormat(+duration) }</p>
+        <h4 className="text-[1.25rem]">
+          Depart{" "}
+          {formatInTimeZone(+departure.scheduled, timeZone, "eee, MMM d, yyyy")}
+        </h4>
+        <p className="font-medium opacity-75">{minutesToHMFormat(+duration)}</p>
       </div>
-      <div className="mb-[40px] grid justify-between gap-[20px] md:flex">
-        <div className="flex w-fit items-center gap-[24px] rounded-[8px] border border-primary px-[32px] py-[16px]">
+      <div className="mb-[40px] grid justify-start gap-[20px] md:flex">
+        <div className="flex w-fit flex-wrap items-center justify-center gap-[24px] rounded-[8px] border border-primary px-[32px] py-[16px]">
           <Image
-            height={ 40 }
-            width={ 60 }
-            src={ airlinesLogos[airline.iataCode] }
+            height={40}
+            width={60}
+            src={airlinesLogos[airline.iataCode]}
             alt="emirates_logo"
-            className="h-[40px] w-auto"
+            className="h-16 w-auto"
           />
           <div>
             <h3 className="mb-[8px] text-[1.5rem] font-semibold">
-              { airline.name }
+              {airline.name}
             </h3>
             <p className="text-[0.875rem] font-medium opacity-60">
-              { airplane.model }
+              {airplane.model}
             </p>
           </div>
         </div>
-        <div className="flex gap-[20px] px-[24px] py-[16px]">
-          <Image src={ plane } height={ 24 } width={ 24 } alt="plane_icon" />
-          <Separator orientation="vertical" />
-          <Image src={ wifi } height={ 24 } width={ 24 } alt="wifi_icon" />
-          <Separator orientation="vertical" />
-          <Image src={ stopwatch } height={ 24 } width={ 24 } alt="stopwatch_icon" />
-          <Separator orientation="vertical" />
-          <Image src={ fastFood } height={ 24 } width={ 24 } alt="fast_food_icon" />
-          <Separator orientation="vertical" />
-          <Image
-            src={ airLineSeat }
-            height={ 24 }
-            width={ 24 }
-            alt="air-plane-seat_icon"
-          />
-        </div>
+        {/* <div>
+          <p>
+            Flight Number:{" "}
+            <span className="font-bold">{flight.flightNumber}</span>
+          </p>
+          <p>
+            Available Seats:{" "}
+            <span className="font-bold">{totalAvailableSeats}</span>
+          </p>
+        </div> */}
       </div>
-      <div className="grid grid-rows-5 items-center gap-[40px] md:flex md:justify-center lg:gap-[80px]">
-        <div>
-          <span className="text-[1rem] font-semibold lg:text-[1.5rem]">
-            { formatInTimeZone(+departure.scheduled, timezone, "h:mm aaa") }
-          </span>{ " " }
-          <span className="max-lg:text-[0.75rem]">{ `${departure.airport.name}(${departure.airport.iataCode})` }</span>
+      <div className="flex flex-col items-start justify-between gap-3 md:flex-row md:items-center">
+        <div className="flex flex-col font-semibold">
+          <span className="text-[1rem] lg:text-[1.5rem]">
+            {formatInTimeZone(+departure.scheduled, timeZone, "h:mm aaa")}
+          </span>{" "}
+          <span className="lg:text-[1.5rem]">
+            {formatInTimeZone(
+              +departure.scheduled,
+              timeZone,
+              "eee, MMM d, yyyy",
+            )}
+          </span>{" "}
+          <span className="max-lg:text-[0.75rem]">{`${departure.airport.name}(${departure.airport.iataCode})`}</span>
+          <span className="max-lg:text-[0.75rem]">{`Terminal: ${departure.terminal}`}</span>
+          <span className="max-lg:text-[0.75rem]">{`Gate: ${departure.gate}`}</span>
         </div>
-        <div className="row-span-3 flex h-max w-max items-center gap-[32px] max-md:rotate-90 md:gap-[24px]">
+        <div className="flex grow items-center justify-center gap-4 max-md:flex-col">
           <Image
-            src={ lineLeft }
-            width={ 36 }
-            height={ 36 }
-            className="w-1/3"
+            src={lineLeft}
+            width={36}
+            height={36}
+            className="min-h-[36px] min-w-[36px] max-md:rotate-90"
             alt="lineleft_icon"
           />
           <Image
-            src={ plane }
+            src={plane}
             alt="plane_icon"
-            className="h-[48px] w-1/3"
-            height={ 48 }
-            width={ 48 }
+            className="min-h-[48px] min-w-[48px] max-md:rotate-90"
+            height={48}
+            width={48}
           />
           <Image
-            className="w-1/3"
-            width={ 36 }
-            height={ 36 }
-            src={ lineRight }
+            className="min-h-[36px] min-w-[36px] max-md:rotate-90"
+            width={36}
+            height={36}
+            src={lineRight}
             alt="lineright_icon"
           />
         </div>
-        <div>
-          <span className="font-semibold lg:text-[1.5rem]">{ formatInTimeZone(+arrival.scheduled, timezone, "h:mm aaa") }</span>{ " " }
-          <span className="max-lg:text-[0.75rem]">{ `${arrival.airport.name}(${arrival.airport.iataCode})` }</span>
+        <div className="flex flex-col font-semibold">
+          <span className="lg:text-[1.5rem]">
+            {formatInTimeZone(+arrival.scheduled, timeZone, "h:mm aaa")}
+          </span>{" "}
+          <span className="lg:text-[1.5rem]">
+            {formatInTimeZone(+arrival.scheduled, timeZone, "eee, MMM d, yyyy")}
+          </span>{" "}
+          <span className="max-lg:text-[0.75rem]">{`${arrival.airport.name} (${arrival.airport.iataCode})`}</span>
+          <span className="max-lg:text-[0.75rem]">{`Terminal: ${arrival.terminal}`}</span>
+          <span className="max-lg:text-[0.75rem]">{`Gate: ${arrival.gate}`}</span>
         </div>
       </div>
+      <Separator className="my-3" />
+      <Dropdown
+        className={"rounded-md bg-gray-100 px-3"}
+        title={<h4 className="text-lg font-bold">Baggage Details</h4>}
+      >
+        {/* Will be added later */}
+        {/* <table>
+          <thead>
+            <tr>
+              <th className="p-2">Baggage</th>
+              <th className="p-2">Weight</th>
+              <th className="p-2">Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {flight.baggage.map((baggage) => (
+              <tr key={baggage._id}>
+                <td className="p-2">{baggage.name}</td>
+                <td className="p-2">{baggage.weight}</td>
+                <td className="p-2">{baggage.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table> */}
+      </Dropdown>
     </div>
   );
 }
