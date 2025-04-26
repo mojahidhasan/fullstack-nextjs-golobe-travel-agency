ee58e2578c35770167862063907d597ee6749761
Mojahid <c.mojahid.me@gmail.com>
2025-04-27 00:51:01 +0600

    feat: update RatingShow component to display rating conditionally based on integer check

--- components/local-ui/ratingShow.js
+++ components/local-ui/ratingShow.js
@@ -2,7 +2,9 @@ import { Button } from "../ui/button";
 export function RatingShow({ rating }) {
   return (
     <Button variant="outline" size="sm" className="rounded-lg px-3 py-1">
-      {Math.floor(rating) < 1 ? rating.toFixed(1) : "N/A"}
+      {Number.isInteger(rating) && Math.floor(rating) > 0
+        ? rating.toFixed(1)
+        : "N/A"}
     </Button>
   );
 }
