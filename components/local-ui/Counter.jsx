4929b825654d9adcab733bbd455915a4f2806d28
Mojahid <c.mojahid.me@gmail.com>
2025-04-27 00:41:20 +0600

    feat: refactor Counter component for improved readability and structure

--- components/local-ui/Counter.jsx
+++ components/local-ui/Counter.jsx
@@ -27,10 +27,23 @@ export default function Counter({
     setCount(c);
     getCount(c);
   }
-  return <div className={ cn("flex w-fit items-center gap-2 h-8", className) }>
-    <button className={ "w-fit h-full" } onClick={ handleDecrement }><MinusCircle width={ 24 } height={ 24 } className={ "w-full h-full text-primary" } /></button>
-    <p className={ "h-fit w-8 text-center text-md font-bold" }>{ count }</p>
-    <button className={ "w-fit h-full" } onClick={ handleIncrement }><PlusCircle width={ 24 } height={ 24 } className={ "w-full h-full text-primary" } /></button>
-  </div>;
-
+  return (
+    <div className={cn("flex h-8 w-fit items-center gap-2", className)}>
+      <button className={"h-full w-fit"} onClick={handleDecrement}>
+        <MinusCircle
+          width={24}
+          height={24}
+          className={"h-full w-full text-primary"}
+        />
+      </button>
+      <p className={"text-md h-fit w-8 text-center font-bold"}>{count}</p>
+      <button className={"h-full w-fit"} onClick={handleIncrement}>
+        <PlusCircle
+          width={24}
+          height={24}
+          className={"h-full w-full text-primary"}
+        />
+      </button>
+    </div>
+  );
 }
