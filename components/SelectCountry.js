bcdd504c83f2e84dde2b9c752309e61d79244ec6
Mojahid <c.mojahid.me@gmail.com>
2025-05-02 21:08:36 +0600

    add error handling to SelectCountry component

--- components/SelectCountry.js
+++ components/SelectCountry.js
@@ -1,6 +1,7 @@
 "use client";
 import countryInfo from "@/data/countryInfo.json";
 import { Option, Select } from "./local-ui/Select";
+import { cn } from "@/lib/utils";
 export function SelectCountry({
   error,
   className,
@@ -9,25 +10,28 @@ export function SelectCountry({
   ...props
 }) {
   return (
-    <Select
-      onValueChange={(val) => {
-        getSelected(val);
-      }}
-      value={value}
-      placeholder="select country"
-      className={className}
-      {...props}
-    >
-      {countryInfo.map((country) => (
-        <Option
-          searchableValue={country.name}
-          className="py-3"
-          key={country.name}
-          value={country.name}
-        >
-          {country.name}
-        </Option>
-      ))}
-    </Select>
+    <>
+      <Select
+        onValueChange={(val) => {
+          getSelected(val);
+        }}
+        value={value}
+        placeholder="select country"
+        className={cn(className, error && "border-destructive")}
+        {...props}
+      >
+        {countryInfo.map((country) => (
+          <Option
+            searchableValue={country.name}
+            className="py-3"
+            key={country.name}
+            value={country.name}
+          >
+            {country.name}
+          </Option>
+        ))}
+      </Select>
+      <p className="mt-1 pl-4 font-medium text-destructive">{error}</p>
+    </>
   );
 }
