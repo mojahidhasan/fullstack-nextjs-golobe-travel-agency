67c5396126924d43314f12eef2599e9c55315ba8
Mojahid <c.mojahid.me@gmail.com>
2025-05-02 21:10:36 +0600

    add setStep prop to ProgressStepper for backward step navigation

--- components/local-ui/ProgressStepper.jsx
+++ components/local-ui/ProgressStepper.jsx
@@ -2,7 +2,12 @@
 
 import { cn } from "@/lib/utils";
 
-function ProgressStepper({ steps, currentStep, className }) {
+function ProgressStepper({
+  steps,
+  currentStep,
+  className,
+  setStep = () => {},
+}) {
   return (
     <div
       className={cn(
@@ -13,28 +18,24 @@ function ProgressStepper({ steps, currentStep, className }) {
       {steps.map((step, index) => (
         <div key={index} className="relative flex flex-1 flex-col items-center">
           <div
-            className={`z-[2] flex h-6 w-6 items-center justify-center rounded-full text-xs font-semibold md:h-8 md:w-8 md:text-base ${
-              index + 1 === currentStep
+            title={step}
+            className={cn(
+              `z-[2] flex h-6 w-6 cursor-pointer items-center justify-center rounded-full text-xs font-semibold md:h-8 md:w-8 md:text-base`,
+              index + 1 <= currentStep
                 ? "bg-primary text-white"
-                : index + 1 < currentStep
-                  ? "bg-primary text-white"
-                  : "bg-[#e7e8e9] text-[#8b8b8b]"
-            }`}
+                : "bg-[#e7e8e9] text-[#8b8b8b]",
+            )}
+            onClick={() => {
+              if (index + 1 < currentStep) {
+                setStep(index + 1);
+              }
+            }}
           >
             {index + 1}
           </div>
           <div className="mt-1 hidden text-center text-[10px] text-[#112211] xsm:block md:mt-2 md:text-sm">
             {step}
           </div>
-          {/* Show only step number on very small screens */}
-          <div className="mt-1 text-center text-[10px] text-[#112211] xsm:hidden">
-            Step {index + 1}
-          </div>
-          {index < steps.length - 1 && (
-            <div
-              className={`absolute left-[calc(50%+10px)] top-3 z-[1] h-[1px] w-[calc(100%-20px)] md:left-[calc(50%+16px)] md:top-4 md:h-[2px] md:w-[calc(100%-32px)] ${index + 1 < currentStep ? "bg-primary" : "bg-[#e7e8e9]"}`}
-            />
-          )}
         </div>
       ))}
     </div>
