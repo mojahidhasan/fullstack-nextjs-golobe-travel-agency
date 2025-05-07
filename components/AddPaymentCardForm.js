284bc44515e69be5043fa2f243ea0a0690f21fe8
Mojahid <c.mojahid.me@gmail.com>
2025-05-07 15:04:51 +0600

    refactor: simplify AddPaymentCardForm structure and enhance payment method selection

--- components/AddPaymentCardForm.js
+++ components/AddPaymentCardForm.js
@@ -1,166 +1,118 @@
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
+"use client";
 import { Checkbox } from "@/components/ui/checkbox";
-import { Button } from "@/components/ui/button";
-import {
-  SelectShadcn,
-  SelectContent,
-  SelectItem,
-  SelectGroup,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-import {
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-} from "@/components/ui/popover";
-
+import { Input } from "./local-ui/input";
+import { useFormState } from "react-dom";
+import { SubmitBtn } from "./local-ui/SubmitBtn";
+import { useReducer } from "react";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+import { Label } from "./ui/label";
 import Image from "next/image";
+import visaIcon from "@/public/icons/visa.svg";
+import masterCardIcon from "@/public/icons/mastercard.svg";
+import paypalIcon from "@/public/icons/paypal.svg";
+import { Button } from "./ui/button";
+export function AddPaymentCardForm({ serverAction = () => {} }) {
+  const [state, dispatch] = useFormState(serverAction, undefined);
+  const paymentMethods = [
+    { icon: visaIcon, label: "Visa", value: "visa" },
+    { icon: masterCardIcon, label: "Master Card", value: "master card" },
+    { icon: paypalIcon, label: "Paypal", value: "paypal" },
+  ];
+  const defaultPaymentForm = (
+    <div className="flex h-[300px] items-center justify-center font-bold">
+      Choose a payment Method
+    </div>
+  );
+  const [reducerState, dispatchReducer] = useReducer(
+    handlePaymentReducer,
+    defaultPaymentForm,
+  );
 
-import visa from "@/public/icons/visa.svg";
-export function AddPaymentCardForm() {
+  function handlePaymentReducer(prevState, action) {
+    switch (action.type) {
+      case "visa":
+        return <InputFields />;
+      case "master card":
+        return <InputFields />;
+      case "paypal":
+        return (
+          <div className="mx-auto">
+            <Button type="button" size="sm">
+              Pay with Paypal
+            </Button>
+          </div>
+        );
+      default:
+        return defaultPaymentForm;
+    }
+  }
   return (
-    <form action="" className="mt-5 flex flex-col gap-6">
-      <div className="grid grid-cols-2 gap-[24px]">
-        <Label
-          className={
-            "relative focus-within:border-blue-500 flex gap-1 rounded-[8px] border-2 border-secondary col-span-2 h-auto text-secondary"
-          }
-        >
-          <span className="absolute -top-[10px] left-[5%] block bg-white px-[4px] text-[0.875rem]">
-            Card Number
-          </span>
-          <Input
-            className="h-[40px] rounded-[8px] border-0 w-full bg-white px-[16px] py-[8px] text-[1rem] outline-none placeholder:text-secondary/50 max-lg:placeholder:text-[0.875rem] lg:h-[56px]"
-            name={"cardNumber"}
-            placeholder={"Add card number"}
-            min={0}
-            required
-          />
-          <Popover>
-            <PopoverTrigger>
-              <Image
-                className="mr-2"
-                src={visa}
-                width={24}
-                height={16}
-                alt="visa_icon"
+    <form id="payment-form" action={dispatch} className="flex flex-col gap-6">
+      <RadioGroup
+        onValueChange={(v) => dispatchReducer({ type: v })}
+        defaultValue="default"
+        name="paymentMethod"
+        form="payment-form"
+        className="grid cursor-pointer grid-cols-1 flex-wrap xsm:grid-cols-2 sm:grid-cols-3"
+      >
+        {paymentMethods.map((method) => {
+          return (
+            <Label
+              key={method.value}
+              htmlFor={method.value}
+              className="flex grow cursor-pointer items-center justify-between gap-[32px] rounded-[12px] border-2 border-primary p-[16px] has-[[data-state='checked']]:bg-primary"
+            >
+              <div className="flex items-center gap-3">
+                <Image
+                  width={24}
+                  height={16}
+                  src={method.icon}
+                  alt={method.icon + "_icon"}
+                />
+                <p className="text-[0.875rem]">{method.value}</p>
+              </div>
+              <RadioGroupItem
+                className="border-2 data-[state='checked']:border-white data-[state='checked']:text-white"
+                value={method.value}
+                id={method.value}
               />
-            </PopoverTrigger>
-            <PopoverContent>
-              <div className="flex">
-                <Image
-                  className="mr-2"
-                  src={visa}
-                  width={24}
-                  height={16}
-                  alt="visa_icon"
-                />
-                <span>Visa</span>
-              </div>
-              <div className="flex">
-                <Image
-                  className="mr-2"
-                  src={visa}
-                  width={24}
-                  height={16}
-                  alt="visa_icon"
-                />
-                <span>Paypal</span>
-              </div>
-              <div className="flex">
-                <Image
-                  className="mr-2"
-                  src={visa}
-                  width={24}
-                  height={16}
-                  alt="visa_icon"
-                />
-                <span>Master card</span>
-              </div>
-            </PopoverContent>
-          </Popover>
-        </Label>
-        {/* <p className={error?.[name] ? "text-error" : ""}>
-                                {error?.[name]}
-                            </p> */}
-        <Label
-          className={
-            "relative focus-within:border-blue-500 flex gap-1 rounded-[8px] border-2 border-secondary col-span-1 h-auto text-secondary"
-          }
-        >
-          <span className="absolute -top-[10px] left-[5%] block bg-white px-[4px] text-[0.875rem]">
-            Exp. Date
-          </span>
-          <Input placeholder={"12/12"} className={"outline-none"} />
-        </Label>
+            </Label>
+          );
+        })}
+      </RadioGroup>
+      {reducerState}
+    </form>
+  );
+}
 
-        {/* <p className={error?.[name] ? "text-error" : ""}>
-                                {error?.[name]}
-                            </p> */}
-        <Label
-          className={
-            "relative focus-within:border-blue-500 flex gap-1 rounded-[8px] border-2 border-secondary col-span-1 h-auto text-secondary"
-          }
-        >
-          <span className="absolute -top-[10px] left-[5%] block bg-white px-[4px] text-[0.875rem]">
-            CVC
-          </span>
-          <Input placeholder={"123"} className={"outline-none"} />
-        </Label>
-        {/* <p className={error?.[name] ? "text-error" : ""}>
-                                {error?.[name]}
-                            </p> */}
-        <Label
-          className={
-            "relative focus-within:border-blue-500 flex gap-1 rounded-[8px] border-2 border-secondary col-span-2 h-auto text-secondary"
-          }
-        >
-          <span className="absolute -top-[10px] left-[5%] block bg-white px-[4px] text-[0.875rem]">
-            Name on card
-          </span>
-          <Input placeholder={"Name on the card"} className={"outline-none"} />
-        </Label>
-        {/* <p className={error?.[name] ? "text-error" : ""}>
-                                {error?.[name]}
-                            </p> */}
-        <Label
-          className={
-            "relative focus-within:border-blue-500 flex gap-1 rounded-[8px] border-2 border-secondary col-span-2 h-auto text-secondary"
-          }
-        >
-          <span className="absolute -top-[10px] left-[5%] block bg-white px-[4px] text-[0.875rem]">
-            Country or region
-          </span>
-          <SelectShadcn>
-            <SelectTrigger className="focus-within::ring-0 bg-white hover:bg-slate-500/10 w-full h-full border-0 ring-blue-700">
-              <SelectValue
-                className="lg:h-[56px] h-[40px]"
-                defaultValue={"Bangladesh"}
-                placeholder={"Bangladesh"}
-              />
-            </SelectTrigger>
-            <SelectContent className={"bg-primary"}>
-              <SelectGroup>
-                <SelectItem value="Bangladesh">Bangladesh</SelectItem>
-                <SelectItem value="India">India</SelectItem>
-                <SelectItem value="Pakistan">Pakistan</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </SelectShadcn>
-        </Label>
+function InputFields() {
+  return (
+    <>
+      <div className="grid grid-cols-1 gap-3 sm:grid-cols-2">
+        <Input
+          name="cardNumber"
+          placeholder="Card Number"
+          label="Card Number"
+        />
+        <Input name="exp" placeholder="mm/yy" label="Expire on" />
+        <Input name="cvc" placeholder="CVV" label="CVV" />
+        <Input
+          name="cardholderName"
+          placeholder="Cardholder Name"
+          label="Cardholder Name"
+        />
       </div>
       <div>
         <Checkbox
           id={"securlySave"}
-          name={"securlySave"}
+          name={"shouldSave"}
           label={"Securely save my information for 1-click checkout"}
         />
       </div>
-      <Button className={"w-full"} type="submit">
-        Add Card
-      </Button>
-    </form>
+      <SubmitBtn
+        formId={"payment-form"}
+        customTitle={{ default: "Pay now", onSubmitting: "Paying..." }}
+      />
+    </>
   );
 }
