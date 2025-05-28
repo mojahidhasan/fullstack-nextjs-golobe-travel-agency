ff787ce452012d2a7d2560d2f23b9a6352ca9650
Mojahid <c.mojahid.me@gmail.com>
2025-05-29 02:56:38 +0600

    refactor: update AccountDetails component for improved readability and consistency in userDetails property access

--- components/pages/profile/AccountDetails.jsx
+++ components/pages/profile/AccountDetails.jsx
@@ -11,44 +11,58 @@ import { AddAnotherEmailPopup } from "./ui/AddAnotherEmailPopup";
 export function AccountDetails({ userDetails }) {
   const sendAgainAt = cookies().get("sai")?.expires?.toISOString();
   const accountDetails = {
-    name: userDetails.firstname + " " + userDetails.lastname,
+    name: userDetails.firstName + " " + userDetails.lastName,
     emails: userDetails.emails,
     phone: userDetails?.phone,
     address: userDetails?.address,
     dateOfBirth: userDetails?.dateOfBirth,
   };
   return (
-    <TabContentMockup title={ "Account" }>
-      <div className={ "bg-white flex flex-col gap-3 p-5 shadow-md rounded-2" }>
+    <TabContentMockup title={"Account"}>
+      <div className={"rounded-2 flex flex-col gap-3 bg-white p-5 shadow-md"}>
         <div>
           <h4 className="opacity-75">Name</h4>
           <div className="flex items-center justify-between">
-            <p className="text-[1.25rem] font-semibold">{ accountDetails.name }</p>
-            <ChangeNamePopup firstname={ userDetails.firstname } lastname={ userDetails.lastname } />
+            <p className="text-[1.25rem] font-semibold">
+              {accountDetails.name}
+            </p>
+            <ChangeNamePopup
+              firstname={userDetails.firstname}
+              lastname={userDetails.lastname}
+            />
           </div>
         </div>
         <div>
           <h4 className="opacity-75">Email</h4>
-          <div className={ "flex flex-col gap-3 items-start lg:flex-row lg:justify-between lg:items-center" }>
+          <div
+            className={
+              "flex flex-col items-start gap-3 lg:flex-row lg:items-center lg:justify-between"
+            }
+          >
             <div>
-              { accountDetails?.emails.map((item) => {
+              {accountDetails?.emails.map((item) => {
                 return (
-                  <div key={ item.email } className="text-[1.25rem] font-semibold flex flex-wrap items-center gap-1">
-                    <span>{ item.email }</span>
-                    { item.primary === true && (
-                      <span className={ "text-sm" }>(primary)</span>
-                    ) }
-                    { item.emailVerifiedAt === null && (
-                      <VerifyEmailBtn email={ item.email } sendAgainAt={ sendAgainAt } />
-                    ) }
+                  <div
+                    key={item.email}
+                    className="flex flex-wrap items-center gap-1 text-[1.25rem] font-semibold"
+                  >
+                    <span>{item.email}</span>
+                    {item.primary === true && (
+                      <span className={"text-sm"}>(primary)</span>
+                    )}
+                    {item.emailVerifiedAt === null && (
+                      <VerifyEmailBtn
+                        email={item.email}
+                        sendAgainAt={sendAgainAt}
+                      />
+                    )}
                   </div>
                 );
-              }) }
-
+              })}
             </div>
             <div className="flex gap-2">
               <AddAnotherEmailPopup />
-              <ChangeEmailPopup emails={ accountDetails.emails } />
+              <ChangeEmailPopup emails={accountDetails.emails} />
             </div>
           </div>
         </div>
@@ -63,7 +77,7 @@ export function AccountDetails({ userDetails }) {
           <h4 className="opacity-75">Phone</h4>
           <div className="flex items-center justify-between">
             <p className="text-[1.25rem] font-semibold">
-              { accountDetails.phone ?? "N/A" }
+              {accountDetails.phone ?? "N/A"}
             </p>
             <ChangePhonePopup />
           </div>
@@ -72,7 +86,7 @@ export function AccountDetails({ userDetails }) {
           <h4 className="opacity-75">Address</h4>
           <div className="flex items-center justify-between">
             <p className="text-[1.25rem] font-semibold">
-              { accountDetails.address ?? "N/A" }
+              {accountDetails.address ?? "N/A"}
             </p>
             <ChangeAddressPopup />
           </div>
@@ -81,7 +95,7 @@ export function AccountDetails({ userDetails }) {
           <h4 className="opacity-75">Date of birth</h4>
           <div className="flex items-center justify-between">
             <p className="text-[1.25rem] font-semibold">
-              { accountDetails.dateOfBirth ?? "N/A" }
+              {accountDetails.dateOfBirth ?? "N/A"}
             </p>
             <ChangeDateOfBirthPopup />
           </div>
