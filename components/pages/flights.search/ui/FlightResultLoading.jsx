d910dff01d44a37b313d226df183b3f71c261f6b
Mojahid <c.mojahid.me@gmail.com>
2025-05-29 00:12:59 +0600

    style: adjust loading skeleton dimensions and spacing in FlightResultLoading component

--- components/pages/flights.search/ui/FlightResultLoading.jsx
+++ components/pages/flights.search/ui/FlightResultLoading.jsx
@@ -3,16 +3,16 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function FlightResultLoadingSkeleton() {
   return (
     <>
-      <div className={ "w-full" }>
-        <Skeleton className="h-[80px] w-full" />
-        <div className="flex my-10 justify-between gap-4">
-          <Skeleton className={ "h-5 w-[40%]" } />
-          <Skeleton className={ "h-5 w-[40%]" } />
+      <div className={"w-full"}>
+        <Skeleton className="h-[50px] w-full" />
+        <div className="my-10 flex justify-between gap-4">
+          <Skeleton className={"h-5 w-[40%]"} />
+          <Skeleton className={"h-5 w-[40%]"} />
         </div>
-        <div className="grid grid-cols-1 mb-5 gap-[16px] sm:max-md:grid-cols-2">
-          { Array.from({ length: 4 }).map((_, index) => (
-            <FlightResultSkeleton key={ index } />
-          )) }
+        <div className="mb-5 grid grid-cols-1 gap-[16px] sm:max-md:grid-cols-2">
+          {Array.from({ length: 4 }).map((_, index) => (
+            <FlightResultSkeleton key={index} />
+          ))}
         </div>
       </div>
     </>
