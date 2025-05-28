f1d4bd91fc0c64bd55ef51c0e6ab1cc294c74ff9
Mojahid <c.mojahid.me@gmail.com>
2025-05-29 02:25:20 +0600

    update: progress stepper ui and changed some functionalities..

--- components/local-ui/ProgressStepper.jsx
+++ components/local-ui/ProgressStepper.jsx
@@ -4,40 +4,60 @@ import { cn } from "@/lib/utils";
 
 function ProgressStepper({
   steps,
-  currentStep,
+  currentStepValue,
+  onCurrentValueChange = () => {},
   className,
-  setStep = () => {},
 }) {
   return (
     <div
       className={cn(
-        "flex w-full items-center justify-between py-3 md:py-5",
+        "relative flex w-full items-center justify-between py-3 md:py-5",
         className,
       )}
     >
-      {steps.map((step, index) => (
-        <div key={index} className="relative flex flex-1 flex-col items-center">
+      {steps.map((step, index) => {
+        const currentStepIndex = steps.findIndex(
+          (s) => s.value === currentStepValue,
+        );
+        const isLast = index === steps.length - 1;
+
+        return (
           <div
-            title={step}
-            className={cn(
-              `z-[2] flex h-6 w-6 cursor-pointer items-center justify-center rounded-full text-xs font-semibold md:h-8 md:w-8 md:text-base`,
-              index + 1 <= currentStep
-                ? "bg-primary text-white"
-                : "bg-[#e7e8e9] text-[#8b8b8b]",
-            )}
-            onClick={() => {
-              if (index + 1 < currentStep) {
-                setStep(index + 1);
-              }
-            }}
+            key={step.value}
+            className="relative flex flex-1 flex-col items-center"
           >
-            {index + 1}
+            <div className="relative z-[2] flex w-full items-center justify-center">
+              <div
+                title={step.label}
+                className={cn(
+                  `flex h-6 w-6 cursor-pointer select-none items-center justify-center rounded-full text-xs font-semibold md:h-8 md:w-8 md:text-base`,
+                  index <= currentStepIndex
+                    ? "bg-primary text-white"
+                    : "bg-[#e7e8e9] text-[#8b8b8b]",
+                )}
+                onClick={() => {
+                  onCurrentValueChange(step.value);
+                }}
+              >
+                {index + 1}
+              </div>
+
+              {!isLast && (
+                <div
+                  className={cn(
+                    "absolute left-[60%] top-1/2 -z-10 h-0.5 w-[80%] -translate-y-1/2 rounded-full md:h-1",
+                    index < currentStepIndex ? "bg-primary" : "bg-[#e7e8e9]",
+                  )}
+                ></div>
+              )}
+            </div>
+
+            <div className="mt-1 text-center text-[10px] text-[#112211] md:mt-2 md:text-sm">
+              {step.label}
+            </div>
           </div>
-          <div className="mt-1 hidden text-center text-[10px] text-[#112211] xsm:block md:mt-2 md:text-sm">
-            {step}
-          </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
