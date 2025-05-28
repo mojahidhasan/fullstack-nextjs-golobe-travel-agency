0e04dd47de671f5e4095c4a08052aa5062f2ab5d
Mojahid <c.mojahid.me@gmail.com>
2025-05-29 03:18:31 +0600

    fix: adjust dialog component layout to ensure close button is properly positioned and children are displayed correctly

--- components/ui/dialog.jsx
+++ components/ui/dialog.jsx
@@ -38,13 +38,15 @@ const DialogContent = React.forwardRef(
           )}
           {...props}
         >
-          {children}
           {close && (
-            <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
-              <X className="h-4 w-4" />
-              <span className="sr-only">Close</span>
-            </DialogPrimitive.Close>
+            <div>
+              <DialogPrimitive.Close className="float-right h-min w-min rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+                <X className="h-4 w-4" />
+                <span className="sr-only">Close</span>
+              </DialogPrimitive.Close>
+            </div>
           )}
+          {children}
         </DialogPrimitive.Content>
       </DialogOverlay>
     </DialogPortal>
