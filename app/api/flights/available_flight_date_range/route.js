a6a532ca6df081146a82198fc4873d6d39fbb728
Mojahid <c.mojahid.me@gmail.com>
2025-05-29 09:22:09 +0600

    refactor: rename getFlightDateRange to getAvailableFlightDateRange for clarity

--- app/api/flights/available_flight_date_range/route.js
+++ app/api/flights/available_flight_date_range/route.js
@@ -1,6 +1,6 @@
-import { getFlightDateRange } from "@/lib/controllers/flights";
+import { getAvailableFlightDateRange } from "@/lib/controllers/flights";
 export async function GET(req) {
-  const flightDateRange = await getFlightDateRange();
+  const flightDateRange = await getAvailableFlightDateRange();
   return Response.json(flightDateRange);
 }
 
