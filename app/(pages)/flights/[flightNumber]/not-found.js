bcfdd9178923b62a9b1fd0bba982f4201db0d710
Mojahid <c.mojahid.me@gmail.com>
2025-06-26 02:41:43 +0600

    feat: refactor FlightNotFound component to utilize reusable NotFound component for improved consistency and maintainability

--- app/(pages)/flights/[flightNumber]/not-found.js
+++ app/(pages)/flights/[flightNumber]/not-found.js
@@ -1,22 +1,15 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import routes from "@/data/routes.json";
-export default function NotFound() {
+import NotFound from "@/app/not-found";
+export default function FlightNotFound() {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center gap-5 text-3xl font-black sm:text-5xl">
-      <span className={"px-6 text-center leading-normal"}>
-        {" "}
-        Flight not found
-      </span>
-      <Button
-        asChild
-        variant={"outline"}
-        className={"hover:bg-primary hover:text-white max-sm:h-[40px]"}
-      >
-        <Link className={"max-sm:text-xs"} href={routes.flights.path}>
-          {routes.flights.title}
-        </Link>
-      </Button>
-    </div>
+    <NotFound
+      whatHappened="Flight Not Found"
+      explanation={
+        "Sorry, the flight you're looking for doesn't exist or has been moved. Let's take you back flight search."
+      }
+      navigateTo={{ path: routes.flights.path, title: routes.flights.title }}
+    />
   );
 }
