63b9e9b937697b277eeca0a303472f17cfc742ce
Mojahid <c.mojahid.me@gmail.com>
2025-06-26 03:06:23 +0600

    refactor: enhance FlightBookPage to handle pending bookings and flight availability checks

--- app/(pages)/flights/[flightNumber]/book/page.js
+++ app/(pages)/flights/[flightNumber]/book/page.js
@@ -1,15 +1,17 @@
 import { BreadcrumbUI } from "@/components/local-ui/breadcrumb";
 import { AuthenticationCard } from "@/components/AuthenticationCard";
 import { auth } from "@/lib/auth";
-import { objDeepCompare, parseFlightSearchParams } from "@/lib/utils";
+import { parseFlightSearchParams } from "@/lib/utils";
 
 import { cookies } from "next/headers";
-import { notFound } from "next/navigation";
-import { getFlight } from "@/lib/controllers/flights";
+import { notFound, redirect } from "next/navigation";
 import { getUserDetails } from "@/lib/controllers/user";
 import SessionTimeoutCountdown from "@/components/local-ui/SessionTimeoutCountdown";
-import { getManyDocs } from "@/lib/db/getOperationDB";
+import { getOneDoc } from "@/lib/db/getOperationDB";
 import BookingSteps from "@/components/pages/flights.book/BookingSteps";
+import { getAvailableSeats } from "@/lib/controllers/flights";
+import InfoPage from "@/components/InfoPage";
+
 export default async function FlightBookPage({ params }) {
   const session = await auth();
   const loggedIn = !!session?.user;
@@ -23,32 +25,99 @@ export default async function FlightBookPage({ params }) {
     isBookmarked: false,
     userEmail: session?.user?.email,
   };
-  const airlinePrices = await getManyDocs("AirlineFlightPrice", {}, [
-    "airlinePrices",
-  ]);
 
-  const flight = await getFlight(
-    {
-      flightNumber: params.flightNumber,
-      flightClass: parsedSearchState?.flightClass || flightClass,
-      passengersObj: parsedSearchState?.passengers,
-    },
-    airlinePrices,
+  if (!loggedIn) {
+    return (
+      <main className="mx-auto my-10 w-[90%] text-secondary">
+        <AuthenticationCard className={"mt-4"} />;
+      </main>
+    );
+  }
+
+  const p = params.flightNumber.split("_");
+  const flightCode = p[0];
+  const date = !isNaN(+p[1]) ? +p[1] : p[1];
+
+  const flight = await getOneDoc(
+    "FlightItinerary",
+    { flightCode: flightCode, date: new Date(date) },
+    ["flight"],
   );
 
   if (Object.keys(flight).length === 0) {
     notFound();
   }
 
+  let hasPendingBooking = false;
+  let bookingId = null;
   if (loggedIn) {
     const userDetails = await getUserDetails(session.user.id, 0);
     metaData.isBookmarked = userDetails.flights.bookmarked.some((el) => {
-      return objDeepCompare(el, {
-        flightId: flight._id,
-        flightNumber: flight.flightNumber,
-        flightClass: metaData.flightClass,
-      });
+      return el.flightId._id === flight._id;
     });
+
+    const booking = await getOneDoc(
+      "FlightBooking",
+      {
+        flightItineraryId: flight._id,
+        userId: session.user.id,
+        paymentStatus: "pending",
+        ticketStatus: "pending",
+      },
+      ["userFlightBooking"],
+      0,
+    );
+
+    bookingId = booking._id;
+    hasPendingBooking = Object.keys(booking).length !== 0;
+  }
+
+  if (hasPendingBooking) {
+    return (
+      <InfoPage
+        whatHappened={"You have a pending booking"}
+        explanation={"Go to your dashboard to complete your pending booking."}
+        navigateTo={{
+          path: "/user/my_bookings/flights" + bookingId + "/payment",
+          title: "Confirm Booking",
+        }}
+      />
+    );
+  }
+
+  const isFlightExpired = flight.expireAt < new Date();
+  let isSeatsAvailable = true;
+
+  for (const segment of flight.segmentIds) {
+    const availableSeats = await getAvailableSeats(segment._id, flightClass);
+    if (availableSeats.length === 0) {
+      isSeatsAvailable = false;
+      break;
+    }
+  }
+
+  if (isFlightExpired && !hasPendingBooking) {
+    return (
+      <InfoPage
+        whatHappened={"Flight Expired"}
+        explanation={
+          "The flight you are trying to book has expired. Please try to book another flight."
+        }
+        navigateTo={{ path: "/flights", title: "Search Flights" }}
+      />
+    );
+  }
+
+  if (!isSeatsAvailable && !hasPendingBooking) {
+    return (
+      <InfoPage
+        whatHappened={"No Seats Available"}
+        explanation={
+          "There are no more seats available for this flight. Please try to book another flight."
+        }
+        navigateTo={{ path: "/flights", title: "Search Flights" }}
+      />
+    );
   }
 
   return (
@@ -59,15 +128,12 @@ export default async function FlightBookPage({ params }) {
           redirectionLink="/flights"
           className={"mt-4 rounded-md"}
         />
-        {loggedIn ? (
-          <BookingSteps
-            flight={flight}
-            metaData={metaData}
-            searchStateObj={parsedSearchState}
-          />
-        ) : (
-          <AuthenticationCard className={"mt-4"} />
-        )}
+
+        <BookingSteps
+          flight={flight}
+          metaData={metaData}
+          searchStateObj={parsedSearchState}
+        />
       </main>
     </>
   );
