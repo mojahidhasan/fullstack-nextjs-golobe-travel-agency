7e1ec0a2a91c1a6404234bc3c9993afea1b56a70
Mojahid <c.mojahid.me@gmail.com>
2025-06-26 03:01:25 +0600

    refactor: button inside button iis not allowed thus removed child button

--- components/pages/profile/AddPaymentCard.js
+++ components/pages/profile/AddPaymentCard.js
@@ -101,37 +101,35 @@ export function AddPaymentCard({ className, reloadSection = () => {} }) {
     <div>
       <div className="text-center">
         <Dialog open={opened} onOpenChange={setOpened}>
-          <DialogTrigger asChild>
+          <DialogTrigger asChild title="Add a new card">
             <div
               className={cn(
                 "flex h-[212px] w-[378px] cursor-pointer items-center justify-center rounded-[16px] border-2 border-dashed border-primary",
                 className,
               )}
             >
-              <div className="text-center">
-                <Button asChild variant="icon" className={"h-auto w-auto p-0"}>
-                  <svg
-                    width="50"
-                    height="51"
-                    viewBox="0 0 50 51"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M49 25.5C49 12.25 38.25 1.5 25 1.5C11.75 1.5 1 12.25 1 25.5C1 38.75 11.75 49.5 25 49.5C38.25 49.5 49 38.75 49 25.5Z"
-                      stroke="#8DD3BB"
-                      strokeWidth="2"
-                      strokeMiterlimit="10"
-                    />
-                    <path
-                      d="M25 15.5V35.5M35 25.5H15"
-                      stroke="#8DD3BB"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                </Button>
+              <div className="flex flex-col items-center text-center">
+                <svg
+                  width="50"
+                  height="51"
+                  viewBox="0 0 50 51"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M49 25.5C49 12.25 38.25 1.5 25 1.5C11.75 1.5 1 12.25 1 25.5C1 38.75 11.75 49.5 25 49.5C38.25 49.5 49 38.75 49 25.5Z"
+                    stroke="#8DD3BB"
+                    strokeWidth="2"
+                    strokeMiterlimit="10"
+                  />
+                  <path
+                    d="M25 15.5V35.5M35 25.5H15"
+                    stroke="#8DD3BB"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
                 <p>Add a new card</p>
               </div>
             </div>
