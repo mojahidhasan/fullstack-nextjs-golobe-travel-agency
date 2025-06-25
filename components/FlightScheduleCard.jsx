cf6d41035bfd665a344ce20f84e969967e798b2d
Mojahid <c.mojahid.me@gmail.com>
2025-06-26 02:50:28 +0600

    feat: refactor FlightScheduleCard to improve structure and integrate BaggageDetails component

--- components/FlightScheduleCard.jsx
+++ components/FlightScheduleCard.jsx
@@ -6,16 +6,17 @@ import lineLeft from "@/public/icons/line-left.svg";
 import lineRight from "@/public/icons/line-right.svg";
 import { minutesToHMFormat } from "@/lib/utils";
 import { cn } from "@/lib/utils";
-import { formatInTimeZone } from "date-fns-tz";
 import { Dropdown } from "./local-ui/Dropdown";
-export function FlightDetailsCard({
-  flight,
-  flightScheduleDetails,
+import NoSSR from "./helpers/NoSSR";
+import ShowTimeInClientSide from "./helpers/ShowTimeInClientSide";
+import BaggageDetails from "./pages/flights.[flightId]/sections/BaggageDetails";
+export function FlightScheduleCard({
+  segment,
+  baggageDetails,
+  metaData,
   className,
 }) {
-  const { departure, arrival, duration, airline, airplane, timeZone } =
-    flightScheduleDetails;
-
+  const { from, to, durationMinutes, airlineId, airplaneId } = segment;
   return (
     <div
       className={cn(
@@ -26,25 +27,32 @@ export function FlightDetailsCard({
       <div className="mb-[24px] flex justify-between font-bold">
         <h4 className="text-[1.25rem]">
           Depart{" "}
-          {formatInTimeZone(+departure.scheduled, timeZone, "eee, MMM d, yyyy")}
+          <NoSSR>
+            <ShowTimeInClientSide
+              date={from.scheduledDeparture}
+              formatStr="eee, MMM d, yyyy"
+            />
+          </NoSSR>
         </h4>
-        <p className="font-medium opacity-75">{minutesToHMFormat(+duration)}</p>
+        <p className="font-medium opacity-75">
+          {minutesToHMFormat(+durationMinutes)}
+        </p>
       </div>
       <div className="mb-[40px] grid justify-start gap-[20px] md:flex">
         <div className="flex w-fit flex-wrap items-center justify-center gap-[24px] rounded-[8px] border border-primary px-[32px] py-[16px]">
           <Image
             height={40}
             width={60}
-            src={airlinesLogos[airline.iataCode]}
+            src={airlinesLogos[airlineId.iataCode]}
             alt="emirates_logo"
             className="h-16 w-auto"
           />
           <div>
             <h3 className="mb-[8px] text-[1.5rem] font-semibold">
-              {airline.name}
+              {airlineId.name}
             </h3>
             <p className="text-[0.875rem] font-medium opacity-60">
-              {airplane.model}
+              {airplaneId.model}
             </p>
           </div>
         </div>
@@ -62,18 +70,24 @@ export function FlightDetailsCard({
       <div className="flex flex-col items-start justify-between gap-3 md:flex-row md:items-center">
         <div className="flex flex-col font-semibold">
           <span className="text-[1rem] lg:text-[1.5rem]">
-            {formatInTimeZone(+departure.scheduled, timeZone, "h:mm aaa")}
+            <NoSSR fallback={"h:mm aaa"}>
+              <ShowTimeInClientSide
+                date={from.scheduledDeparture}
+                formatStr="h:mm aaa"
+              />
+            </NoSSR>
           </span>{" "}
           <span className="lg:text-[1.5rem]">
-            {formatInTimeZone(
-              +departure.scheduled,
-              timeZone,
-              "eee, MMM d, yyyy",
-            )}
+            <NoSSR fallback={"eee, MMM d, yyyy"}>
+              <ShowTimeInClientSide
+                date={from.scheduledDeparture}
+                formatStr="eee, MMM d, yyyy"
+              />
+            </NoSSR>
           </span>{" "}
-          <span className="max-lg:text-[0.75rem]">{`${departure.airport.name}(${departure.airport.iataCode})`}</span>
-          <span className="max-lg:text-[0.75rem]">{`Terminal: ${departure.terminal}`}</span>
-          <span className="max-lg:text-[0.75rem]">{`Gate: ${departure.gate}`}</span>
+          <span className="max-lg:text-[0.75rem]">{`${from.airport.name}(${from.airport.iataCode})`}</span>
+          <span className="max-lg:text-[0.75rem]">{`Terminal: ${from.terminal}`}</span>
+          <span className="max-lg:text-[0.75rem]">{`Gate: ${from.gate}`}</span>
         </div>
         <div className="flex grow items-center justify-center gap-4 max-md:flex-col">
           <Image
@@ -100,14 +114,24 @@ export function FlightDetailsCard({
         </div>
         <div className="flex flex-col font-semibold">
           <span className="lg:text-[1.5rem]">
-            {formatInTimeZone(+arrival.scheduled, timeZone, "h:mm aaa")}
+            <NoSSR fallback={"h:mm aaa"}>
+              <ShowTimeInClientSide
+                date={to.scheduledArrival}
+                formatStr="h:mm aaa"
+              />
+            </NoSSR>
           </span>{" "}
           <span className="lg:text-[1.5rem]">
-            {formatInTimeZone(+arrival.scheduled, timeZone, "eee, MMM d, yyyy")}
+            <NoSSR fallback={"eee, MMM d, yyyy"}>
+              <ShowTimeInClientSide
+                date={to.scheduledArrival}
+                formatStr="eee, MMM d, yyyy"
+              />
+            </NoSSR>
           </span>{" "}
-          <span className="max-lg:text-[0.75rem]">{`${arrival.airport.name} (${arrival.airport.iataCode})`}</span>
-          <span className="max-lg:text-[0.75rem]">{`Terminal: ${arrival.terminal}`}</span>
-          <span className="max-lg:text-[0.75rem]">{`Gate: ${arrival.gate}`}</span>
+          <span className="max-lg:text-[0.75rem]">{`${to.airport.name} (${to.airport.iataCode})`}</span>
+          <span className="max-lg:text-[0.75rem]">{`Terminal: ${to.terminal}`}</span>
+          <span className="max-lg:text-[0.75rem]">{`Gate: ${to.gate}`}</span>
         </div>
       </div>
       <Separator className="my-3" />
@@ -115,25 +139,7 @@ export function FlightDetailsCard({
         className={"rounded-md bg-gray-100 px-3"}
         title={<h4 className="text-lg font-bold">Baggage Details</h4>}
       >
-        {/* Will be added later */}
-        {/* <table>
-          <thead>
-            <tr>
-              <th className="p-2">Baggage</th>
-              <th className="p-2">Weight</th>
-              <th className="p-2">Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {flight.baggage.map((baggage) => (
-              <tr key={baggage._id}>
-                <td className="p-2">{baggage.name}</td>
-                <td className="p-2">{baggage.weight}</td>
-                <td className="p-2">{baggage.price}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table> */}
+        <BaggageDetails baggage={baggageDetails} />
       </Dropdown>
     </div>
   );
