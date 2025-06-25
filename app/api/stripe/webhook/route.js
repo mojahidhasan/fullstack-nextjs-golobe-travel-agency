e9174225165afbd46dc0e604ea117cc4ae43b4a2
Mojahid <c.mojahid.me@gmail.com>
2025-06-26 02:40:33 +0600

    feat: implement MongoDB session support for flight payment processing and enhance email data handling

--- app/api/stripe/webhook/route.js
+++ app/api/stripe/webhook/route.js
@@ -6,6 +6,7 @@ import { updateOneDoc } from "@/lib/db/updateOperationDB";
 import sendEmail from "@/lib/email/sendEmail";
 import { flightBookingConfirmedEmailTemplate } from "@/lib/email/templates";
 import initStripe from "@/lib/paymentIntegration/stripe";
+import mongoose from "mongoose";
 
 const stripe = initStripe();
 const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET;
@@ -29,59 +30,81 @@ export async function POST(req) {
     case "charge.updated":
       const charge = event.data.object;
       const methodtype = charge.payment_method_details.type;
-      const flightPayment = {
-        bookingId: charge.metadata.flightBookingId,
-        transactionId: charge.balance_transaction,
-        paymentDate: charge.created,
-        paymentMethod: {
-          id: charge.payment_method,
-          methodType: methodtype,
-          brand: charge.payment_method_details[methodtype].brand,
-          last4: charge.payment_method_details[methodtype].last4,
-        },
-        amount: charge.amount,
-        receiptUrl: charge.receipt_url,
-      };
+      const session = await mongoose.startSession();
+      session.startTransaction();
       try {
         if (charge.metadata?.type === "flightBooking") {
-          const paymentInfo = await createOneDoc(
-            "FlightPayment",
-            flightPayment,
-          );
-          const bookingInfo = await updateOneDoc(
+          const flightPayment = {
+            bookingId: charge.metadata.flightBookingId,
+            transactionId: charge.balance_transaction,
+            paymentDate: charge.created,
+            paymentMethod: {
+              id: charge.payment_method,
+              methodType: methodtype,
+              brand: charge.payment_method_details[methodtype].brand,
+              last4: charge.payment_method_details[methodtype].last4,
+            },
+            amount: charge.amount,
+            receiptUrl: charge.receipt_url,
+          };
+
+          const booking = await getOneDoc(
             "FlightBooking",
             {
               _id: charge.metadata.flightBookingId,
             },
-            {
-              bookingStatus: "confirmed",
-              paymentStatus: "paid",
-              paymentId: paymentInfo._id,
-            },
+            ["userFlightBooking"],
+            0,
           );
-          await assignSeatsToFlightBooking(
-            charge.metadata.flightBookingId,
-            "permanent",
-          );
+          try {
+            const paymentInfo = await createOneDoc(
+              "FlightPayment",
+              flightPayment,
+              { session },
+            );
+            const bookingInfo = await updateOneDoc(
+              "FlightBooking",
+              {
+                _id: charge.metadata.flightBookingId,
+              },
+              {
+                ticketStatus: "confirmed",
+                paymentStatus: "paid",
+                paymentId: paymentInfo._id,
+                bookedAt: new Date(charge.created * 1000),
+              },
+              { session },
+            );
 
-          const getBookingInfo = await getOneDoc("FlightBooking", {
-            _id: charge.metadata.flightBookingId,
+            await assignSeatsToFlightBooking(booking, "permanent", 0, session);
+            await session.commitTransaction();
+          } catch (err) {
+            console.log(err);
+            if (session.inTransaction()) {
+              await session.abortTransaction();
+            }
+            return Response.json({ success: false, message: err.message });
+          } finally {
+            await session.endSession();
+          }
+
+          const flight = await getOneDoc("FlightItinerary", {
+            _id: booking.flightItineraryId,
           });
-
-          const flight = getBookingInfo.flightSnapshot;
           const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+          const { emailFlightDetails, emailBookingDetails } =
+            processFlightBookingDataForEmail(flight, booking);
 
           const htmlEmail = flightBookingConfirmedEmailTemplate({
             ...emailDefaultData,
             main: {
               manageBookingUrl:
-                baseUrl +
-                `/user/my_bookings/flights/${getBookingInfo.bookingRef}`,
+                baseUrl + `/user/my_bookings/flights/${booking._id}`,
               downloadTicketUrl:
-                baseUrl +
-                `/user/my_bookings/flights/${getBookingInfo.bookingRef}/ticket`,
-              flightDetails: flight,
-              bookingDetails: getBookingInfo,
+                baseUrl + `/user/my_bookings/flights/${booking._id}/ticket`,
+              flightDetails: emailFlightDetails,
+              bookingDetails: emailBookingDetails,
             },
           });
           await sendEmail(
@@ -103,8 +126,49 @@ export async function POST(req) {
   return Response.json({ success: true, message: "Success" });
 }
 
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// };
+function processFlightBookingDataForEmail(dbFlightData, dbBookingData) {
+  return {
+    emailFlightDetails: {
+      itineraryFlightNumber: dbFlightData.flightCode,
+      segments: dbFlightData.segmentIds.map((s) => ({
+        flightNumber: s.flightNumber,
+        airlineName: s.airlineId.name,
+        departureDateTime: s.from.scheduledDeparture,
+        departureAirportName: s.from.airport.name,
+        departureAirportIataCode: s.from.airport.iataCode,
+        arrivalDateTime: s.to.scheduledArrival,
+        arrivalAirportName: s.to.airport.name,
+        arrivalAirportIataCode: s.to.airport.iataCode,
+        totalDurationMinutes: s.durationMinutes,
+        airplaneModelName: s.airplaneId.model,
+      })),
+    },
+    emailBookingDetails: {
+      pnrCode: dbBookingData.pnrCode,
+      userTimeZone: dbBookingData.userTimeZone,
+      totalfare: dbBookingData.totalFare,
+      currency: dbBookingData.currency || "USD",
+      bookedAt: dbBookingData.bookedAt || dbBookingData.createdAt,
+      ticketType: "refundable",
+      fareClass: "",
+      paymentMethod: {
+        brand: dbBookingData.paymentId.paymentMethod.brand,
+        last4: dbBookingData.paymentId.paymentMethod.last4,
+        receiptUrl: dbBookingData.paymentId.receiptUrl,
+      },
+      passengers: dbBookingData.passengers.map((p) => {
+        const seat = dbBookingData.selectedSeats.find(
+          (s) => s.passengerId === p._id,
+        );
+
+        return {
+          firstName: p.firstName,
+          lastName: p.lastName,
+          seatNumber: seat.seatId.seatNumber,
+          seatClass: seat.seatId.class,
+          passengerType: p.passengerType,
+        };
+      }),
+    },
+  };
+}
