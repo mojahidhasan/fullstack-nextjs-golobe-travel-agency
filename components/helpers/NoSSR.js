982df5706d8979a99b237f05ebaf512b63e3802c
Mojahid <c.mojahid.me@gmail.com>
2025-06-26 02:50:47 +0600

    feat: enhance NoSSR component to include loading fallback for improved user experience

--- components/helpers/NoSSR.js
+++ components/helpers/NoSSR.js
@@ -4,6 +4,13 @@ import React from "react";
 const NoSsr = (props) => <React.Fragment>{props.children}</React.Fragment>;
 
 // eslint-disable-next-line no-undef
-export default dynamic(() => Promise.resolve(NoSsr), {
-  ssr: false,
-});
+function NoSSR({ children, fallback }) {
+  const DynamicNoSSR = dynamic(() => Promise.resolve(NoSsr), {
+    ssr: false,
+    loading: () => fallback,
+  });
+
+  return <DynamicNoSSR>{children}</DynamicNoSSR>;
+}
+
+export default NoSSR;
