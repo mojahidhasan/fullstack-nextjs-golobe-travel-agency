d7eeec4698d92edfa7e8fb7737320419314b09b4
Mojahid <c.mojahid.me@gmail.com>
2025-07-01 23:09:39 +0600

    refactor: simplify rendering of city and country in HotelDestinationPopover by directly accessing properties from the object

--- components/local-ui/HotelDestinationPopover.jsx
+++ components/local-ui/HotelDestinationPopover.jsx
@@ -12,18 +12,10 @@ export function HotelDestinationPopover({
   getSelected = () => {},
 }) {
   function renderSelectedResult(obj) {
-    if (Object.keys(obj).length > 0) {
-      return (
-        <div className={cn("rounded border p-2", className)}>
-          <div className={"text-2xl font-bold"}>{obj.address.city}</div>
-          <div className={"text-sm"}>{obj.address.country}</div>
-        </div>
-      );
-    }
     return (
       <div className={cn("rounded border p-2", className)}>
-        <div className={"text-2xl font-bold"}>{"City"}</div>
-        <div className={"text-sm"}>{"Country"}</div>
+        <div className={"text-2xl font-bold"}>{obj?.city || "City"}</div>
+        <div className={"text-sm"}>{obj?.country || "Country"}</div>
       </div>
     );
   }
@@ -44,8 +36,8 @@ export function HotelDestinationPopover({
       >
         <Image width={24} height={24} src={locationIcon} alt="location_icon" />
         <div>
-          <div className={"text-md font-bold"}>{obj.address.city}</div>
-          <div className={"text-xs"}>{obj.address.country}</div>
+          <div className={"text-md font-bold"}>{obj.city}</div>
+          <div className={"text-xs"}>{obj.country}</div>
         </div>
       </div>
     ));
