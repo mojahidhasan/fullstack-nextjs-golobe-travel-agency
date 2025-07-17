fdaedc8b63ca304c725c1f47e016953e14d476ba
mojahid <c.mojahid.me@gmail.com>
2025-07-17 21:39:23 +0600

    feat: add BookingReview component for displaying booking details and guest information

---
+++ components/pages/hotels.book/BookingReview.jsx
@@ -0,0 +1,134 @@
+// In development
+"use client";
+
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { format, intervalToDuration } from "date-fns";
+import { useEffect, useState } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import hotelRoomReserveAction from "@/lib/actions/hotelRoomReserveAction";
+
+export default function BookingReview({ nextStep, hotelDetails, searchState }) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const hotelInfo = {
+    hotelName: hotelDetails.name,
+  };
+  const searchInfo = {
+    checkInDate: searchState.checkIn,
+    checkOutDate: searchState.checkOut,
+    nights: intervalToDuration({
+      start: new Date(searchState.checkIn),
+      end: new Date(searchState.checkOut),
+    }).days,
+  };
+
+  const [guestInfo, setGuestInfo] = useState([]);
+  const [selectedRooms, setRooms] = useState([]);
+
+  const strHotelDetails = JSON.stringify(hotelDetails);
+  const strSearchState = JSON.stringify(searchState);
+
+  useEffect(() => {
+    const guestsDetails = JSON.parse(sessionStorage.getItem("guests") || "{}");
+    const selectedRooms = JSON.parse(
+      sessionStorage.getItem("selectedRooms") || "[]",
+    );
+
+    const guestsArr = Object.values(guestsDetails);
+
+    setGuestInfo(guestsArr);
+    setRooms(selectedRooms);
+  }, [strHotelDetails, strSearchState]);
+
+  function formatDate(date) {
+    try {
+      return format(new Date(date), "dd MMM yyyy");
+    } catch {
+      return date;
+    }
+  }
+
+  async function handleConfirm() {
+    const bookingData = {
+      guests: guestInfo,
+      selectedRooms: selectedRooms,
+    };
+    await hotelRoomReserveAction(bookingData);
+    // sessionStorage.removeItem("guests");
+    // sessionStorage.removeItem("selectedRooms");
+    // router.push(`${pathname}?tab=${nextStep}`);
+  }
+  return (
+    <div className="space-y-6">
+      <Card>
+        <CardContent className="space-y-2 p-4">
+          <h2 className="text-xl font-semibold">{hotelInfo.hotelName}</h2>
+          <p className="text-sm text-muted-foreground">
+            {formatDate(searchInfo.checkInDate)} —{" "}
+            {formatDate(searchInfo.checkOutDate)} ({searchInfo.nights} night
+            {searchInfo.nights > 1 && "s"})
+          </p>
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardContent className="space-y-2 p-4">
+          <h3 className="mb-2 text-lg font-bold">Guest Information</h3>
+          {guestInfo.map((guest, index) => (
+            <div key={index} className="space-y-2 pl-2">
+              <h4 className="text-md font-semibold">
+                Guest {index + 1} {guest.isPrimary ? "(Primary)" : ""}
+              </h4>
+              <div className="grid grid-cols-1 gap-3 rounded-md border bg-muted p-3 md:grid-cols-2">
+                <p>
+                  <span className="font-medium">Full Name:</span>{" "}
+                  {guest.firstName} {guest.lastName}
+                </p>
+                {guest.email && (
+                  <p>
+                    <span className="font-medium">Email:</span> {guest.email}
+                  </p>
+                )}
+                {guest.phone && (
+                  <p>
+                    <span className="font-medium">Phone:</span>{" "}
+                    {guest.phone.dialCode} {guest.phone.number}
+                  </p>
+                )}
+                <p className="capitalize">
+                  <span className="font-medium">Guest Type:</span>{" "}
+                  {guest.guestType}
+                </p>
+              </div>
+            </div>
+          ))}
+        </CardContent>
+      </Card>
+      <Card>
+        <CardContent className="space-y-2 p-4">
+          <h3 className="mb-2 text-lg font-medium">Selected Rooms</h3>
+          {selectedRooms.map((room, index) => (
+            <div
+              key={room._id || index}
+              className="flex flex-col justify-between border-b py-3 md:flex-row"
+            >
+              <div>
+                <p className="font-semibold">{room.name}</p>
+                <p className="text-sm text-muted-foreground">
+                  # {room.roomType} | Sleeps {room.sleepsCount} |{" "}
+                  {room.bedOptions}
+                </p>
+              </div>
+            </div>
+          ))}
+        </CardContent>
+      </Card>
+
+      <div className="text-end">
+        <Button onClick={handleConfirm}>Confirm Booking</Button>
+      </div>
+    </div>
+  );
+}
