bb860bd50d5642c8ceaa0d19b4ff50cd254b97bf
mojahid <c.mojahid.me@gmail.com>
2025-07-17 21:39:14 +0600

    feat: implement HotelBookingSteps component with multi-step booking functionality

---
+++ app/(pages)/hotels/[slug]/book/HotelBookingSteps.js
@@ -0,0 +1,87 @@
+// In development
+"use client";
+import { HotelFareCard } from "@/components/FareCard";
+import ProgressStepper from "@/components/local-ui/ProgressStepper";
+import BookingReview from "@/components/pages/hotels.book/BookingReview";
+import GuestInfoForm from "@/components/pages/hotels.book/GuestInfoForm";
+import HotelBookingPayment from "@/components/pages/hotels.book/HotelBookingPayment";
+import { RoomSelector } from "@/components/pages/hotels.book/RoomSelector";
+import { Separator } from "@/components/ui/separator";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+export function HotelBookingSteps({ hotelDetails, searchState }) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const tab = searchParams.get("tab");
+
+  function renderComponent(tab) {
+    switch (tab) {
+      case "guest_info":
+        return (
+          <GuestInfoForm
+            nextStep="select_room"
+            guestsCount={searchState.guests}
+          />
+        );
+      case "select_room":
+        return (
+          <RoomSelector
+            nextStep="review"
+            rooms={hotelDetails.rooms}
+            guests={searchState.guests}
+          />
+        );
+      case "review":
+        return (
+          <BookingReview
+            nextStep="payment"
+            searchState={searchState}
+            hotelDetails={hotelDetails}
+          />
+        );
+      case "payment":
+        return <HotelBookingPayment />;
+      default:
+        return (
+          <GuestInfoForm
+            nextStep={"select_room"}
+            guestsCount={searchState.guests}
+          />
+        );
+    }
+  }
+
+  const bookingSteps = [
+    { label: "Guest Info", value: "guest_info" },
+    { label: "Select Room", value: "select_room" },
+    { label: "Review", value: "review" },
+    { label: "Payment", value: "payment" },
+  ];
+
+  return (
+    <>
+      <ProgressStepper
+        className="my-5"
+        steps={bookingSteps}
+        currentStepValue={tab || "guest_info"}
+        onCurrentValueChange={(value) => {
+          router.push(`${pathname}?tab=${value}`);
+        }}
+      />
+      <div className="flex gap-5 max-lg:flex-col">
+        <div className="w-full">
+          {renderComponent(tab)}
+
+          <Separator className="mt-5" />
+        </div>
+        <div className="flex-grow">
+          <div className="h-auto rounded-lg bg-white p-6 shadow-lg">
+            <h3 className="mb-3 text-2xl font-bold">Fare Summary</h3>
+            <HotelFareCard className="p-0 shadow-none" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
