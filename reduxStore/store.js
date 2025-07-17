83c952492a128916fa534129696348179be5a88b
mojahid <c.mojahid.me@gmail.com>
2025-07-17 21:12:22 +0600

    feat: add hotelRoomsSelector to store for managing hotel room selection state

--- reduxStore/store.js
+++ reduxStore/store.js
@@ -3,12 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import flightFormSlice from "@/reduxStore/features/flightFormSlice";
 import stayFormSlice from "@/reduxStore/features/stayFormSlice";
 import singlePassengerFormSlice from "@/reduxStore/features/singlePassengerFormSlice";
+import hotelRoomsSelectorSlice from "@/reduxStore/features/hotelRoomSelectorSlice";
 export const makeStore = () => {
   return configureStore({
     reducer: {
       flightForm: flightFormSlice,
       stayForm: stayFormSlice,
       singlePassengerForm: singlePassengerFormSlice,
+      hotelRoomsSelector: hotelRoomsSelectorSlice,
     },
   });
 };
