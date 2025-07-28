738a54cb8fc1f0ddd67c96a29a959389a126d7ad
mojahid <c.mojahid.me@gmail.com>
2025-07-28 16:40:57 +0600

    feat: enhance HotelBookingPayment component with payment method selection and cash booking confirmation

--- components/pages/hotels.book/HotelBookingPayment.js
+++ components/pages/hotels.book/HotelBookingPayment.js
@@ -1,4 +1,3 @@
-// In development
 "use client";
 import Countdown from "@/components/local-ui/Countdown";
 import { AlertTriangle, ClockIcon } from "lucide-react";
@@ -8,52 +7,62 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { Button } from "@/components/ui/button";
 import { toast } from "@/components/ui/use-toast";
 import { useRouter } from "next/navigation";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { confirmHotelBookingCashAction } from "@/lib/actions/confirmHotelBookingAction";
 export default function HotelBookingPayment({
-  flightNumber,
-  flightDateTimestamp,
+  slug,
+  checkInDate,
+  checkOutDate,
 }) {
   const router = useRouter();
+  const [paymentMethodType, setPaymentMethodType] = useState("cash");
   const {
-    data: flightBookingData,
-    loading: flightBookingLoading,
-    error: flightBookingError,
-    retry: flightBookingRetry,
+    data: hotelBookingData,
+    loading: hotelBookingLoading,
+    error: hotelBookingError,
+    retry: hotelBookingRetry,
   } = useFetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/user/get_reserved_flight`,
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/user/get_reserved_hotel`,
     {
       method: "POST",
       contentType: "application/json",
       body: JSON.stringify({
-        flightNumber,
-        flightDateTimestamp,
+        slug,
+        checkInDate,
+        checkOutDate,
       }),
     },
   );
+
   const { data, loading, error, retry } = useFetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/stripe/create_hotel_booking_payment_intent`,
     {
       method: "POST",
       contentType: "application/json",
       body: JSON.stringify({
-        flightNumber,
-        flightDateTimestamp,
+        slug,
+        checkInDate,
+        checkOutDate,
       }),
     },
   );
   async function middleware(next = async () => {}) {
     try {
       const res = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/user/get_reserved_flight`,
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/user/get_reserved_hotel`,
         {
           method: "POST",
           body: JSON.stringify({
-            flightNumber: flightNumber,
-            flightDateTimestamp,
+            slug,
+            checkInDate,
+            checkOutDate,
           }),
         },
       );
       if (!res.ok) {
-        throw new Error("Failed to load flight booking");
+        throw new Error("Failed to fetch booking details");
       }
 
       const data = await res.json();
@@ -73,7 +82,7 @@ export default function HotelBookingPayment({
     }
   }
 
-  function onSuccess() {
+  function onSuccessCard() {
     toast({
       title: "Payment successful",
       description: "Your payment was successful, redirecting...",
@@ -84,44 +93,64 @@ export default function HotelBookingPayment({
       const searchParams = new URLSearchParams({
         title: "Payment successful",
         message: "Your payment was successful",
-        callbackUrl: `/user/my_bookings/flights/${flightBookingData.data._id}/ticket`,
-        callbackTitle: "Download ticket",
+        callbackUrl: `/user/my_bookings/hotels/${hotelBookingData.data._id}/invoice`,
+        callbackTitle: "Download Invoice",
+      });
+      router.push(`/success?${searchParams.toString()}`);
+    }, 2000);
+  }
+  function onSuccessCash() {
+    toast({
+      title: "Booking confirmed",
+      description: "Your booking was confirmed, redirecting...",
+      variant: "default",
+    });
+
+    setTimeout(() => {
+      const searchParams = new URLSearchParams({
+        title: "Booking confirmed",
+        message: "Your booking was confirmed. You can pay on property or now",
+        callbackUrl: `/user/my_bookings/hotels/${hotelBookingData.data._id}`,
+        callbackTitle: "View booking",
       });
       router.push(`/success?${searchParams.toString()}`);
     }, 2000);
   }
 
-  return flightBookingError ? (
+  return hotelBookingError ? (
     <div className="flex h-full min-h-[300px] flex-col items-center justify-center gap-6 rounded-md border border-red-300 bg-red-50 p-6 shadow-sm">
       <div className="flex items-center gap-2 text-red-600">
         <AlertTriangle className="h-6 w-6" />
         <h2 className="text-xl font-semibold">Failed loading payment form</h2>
       </div>
       <p className="max-w-xl text-center text-sm text-red-700">
-        {flightBookingError || "Something went wrong. Please try again."}
+        {hotelBookingError || "Something went wrong. Please try again."}
       </p>
-      <Button onClick={flightBookingRetry} variant="destructive">
-        Try Again
-      </Button>
+      {hotelBookingError !== "No reserved hotel booking found" && (
+        <Button onClick={hotelBookingRetry} variant="destructive">
+          Try Again
+        </Button>
+      )}
     </div>
   ) : (
     <>
       <div className="mb-6 rounded-lg border border-yellow-300 bg-yellow-50 p-4 shadow-md">
         <p className="text-sm text-gray-800">
           Your booking has been reserved and will be held for a limited time.
-          Please complete the payment within the limit to confirm and finalize
-          your booking. After the reservation is expired, there is no guarantee
-          that your seat will be reserved and may be taken by someone else.
+          Please confirm the booking within the limit whether by selecting cash
+          payment on the property or by making a payment using card.. After the
+          reservation is expired, there is no guarantee that one of the rooms in
+          your booking will be reserved and may be taken by someone else.
         </p>
         <div className="mt-2 flex items-center gap-2 text-sm font-semibold text-yellow-800">
           <ClockIcon className="h-4 w-4 text-yellow-600" />
           Guaranteed reservation until:{" "}
-          {!flightBookingLoading ? (
+          {!hotelBookingLoading ? (
             <Countdown
               currentTimeMs={Date.now()}
               timeoutAtMs={
                 new Date(
-                  flightBookingData?.data?.guaranteedReservationUntil,
+                  hotelBookingData?.data?.guaranteedReservationUntil,
                 )?.getTime() || 0
               }
             />
@@ -130,15 +159,75 @@ export default function HotelBookingPayment({
           )}
         </div>
       </div>
-      <MakePaymentSection
-        onSuccess={onSuccess}
-        middleware={middleware}
-        loading={loading}
-        error={error}
-        retry={retry}
-        paymentIntents={data?.data?.paymentIntents}
-        paymentStatus={data?.data?.paymentStatus}
-      />
+      <div className="mb-[20px] rounded-[12px] bg-white p-[16px] shadow-md lg:mb-[30px] xl:mb-[40px]">
+        <RadioGroup onValueChange={setPaymentMethodType} defaultValue="cash">
+          <Label className="flex grow items-center justify-between gap-[32px] rounded-[12px] p-[16px] has-[[data-state='checked']]:bg-primary">
+            <div>
+              <p className="mb-2 font-bold">Pay in Property</p>
+              <p className="text-[0.875rem]">
+                Pay at the property when you check-in.
+              </p>
+            </div>
+            <RadioGroupItem
+              className="border-2 data-[state='checked']:border-white data-[state='checked']:text-white"
+              value="cash"
+            />
+          </Label>
+          <Label className="flex grow items-center justify-between gap-[32px] rounded-[12px] p-[16px] has-[[data-state='checked']]:bg-primary">
+            <div>
+              <p className="mb-2 font-bold">Pay now</p>
+              <p className="text-[0.875rem]">
+                Pay now and get your room reserved.
+              </p>
+            </div>
+            <RadioGroupItem
+              className="border-2 data-[state='checked']:border-white data-[state='checked']:text-white"
+              value="card"
+            />
+          </Label>
+        </RadioGroup>
+      </div>
+      <div className="flex justify-center rounded-[12px] bg-white p-[16px] shadow-md lg:mb-[30px] xl:mb-[40px]">
+        {paymentMethodType === "card" && (
+          <MakePaymentSection
+            className={"w-full shadow-none"}
+            onSuccess={onSuccessCard}
+            middleware={middleware}
+            loading={loading}
+            error={error}
+            retry={retry}
+            paymentIntents={data?.data?.paymentIntents}
+            paymentStatus={data?.data?.paymentStatus}
+          />
+        )}
+        {paymentMethodType === "cash" && (
+          <Button
+            onClick={async (e) => {
+              e.target.disabled = true;
+              const { success, message } = await confirmHotelBookingCashAction(
+                hotelBookingData?.data?._id,
+              );
+
+              if (!success) {
+                toast({
+                  title: "Error",
+                  description: message,
+                  variant: "destructive",
+                });
+              }
+              if (success) {
+                onSuccessCash();
+              }
+              e.target.disabled = false;
+            }}
+            variant="default"
+            disabled={loading}
+            className="w-[200px] text-center"
+          >
+            Pay Later
+          </Button>
+        )}
+      </div>
     </>
   );
 }
