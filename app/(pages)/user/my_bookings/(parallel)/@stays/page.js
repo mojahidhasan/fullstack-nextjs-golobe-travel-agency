37f3107e2ef8d0b7dd044a65a18b907a1156a4ee
mojahid <c.mojahid.me@gmail.com>
2025-07-28 17:09:32 +0600

    feat: add loading skeleton component for hotel bookings and make booking list showing functional

--- app/(pages)/user/my_bookings/(parallel)/@stays/page.js
+++ app/(pages)/user/my_bookings/(parallel)/@stays/page.js
@@ -1,21 +1,57 @@
-import { HotelTicketDownloadCard } from "@/components/pages/profile/ui/HotelTicketDownloadCard";
-import CVKHotel from "@/public/images/CVK-hotel.jpg";
-export default function Stays() {
-  const ticketHotel = {
-    id: 1,
-    check: {
-      in: new Date(2023, 1, 1, 18, 0),
-      out: new Date(2023, 1, 1, 22, 0),
-    },
-    roomNo: "On Arrival",
-    logo: CVKHotel,
-  };
+import HotelBookingDetailsCard from "@/components/pages/profile/ui/HotelBookingDetailsCard";
+import { auth } from "@/lib/auth";
+import { getAllHotelBookings } from "@/lib/controllers/hotels";
+import { getOneDoc } from "@/lib/db/getOperationDB";
+import { differenceInDays } from "date-fns";
+
+export default async function Stays() {
+  const session = await auth();
+  const userId = session?.user?.id;
+
+  const bookingData = await getAllHotelBookings(userId);
   return (
-    <div className="flex flex-col gap-3">
-      <HotelTicketDownloadCard ticketData={ticketHotel} />
-      <HotelTicketDownloadCard ticketData={ticketHotel} />
-      <HotelTicketDownloadCard ticketData={ticketHotel} />
-      <HotelTicketDownloadCard ticketData={ticketHotel} />
+    <div className="space-y-4">
+      {bookingData.length === 0 && <Empty />}
+      {bookingData.map(async (booking) => {
+        const hotelDetails = await getOneDoc("Hotel", { _id: booking.hotelId });
+        const data = {
+          key: booking._id,
+          bookingId: booking._id,
+          bookingStatus: booking.bookingStatus,
+          paymentStatus: booking.paymentStatus,
+          bookedAt: new Date(booking.createdAt),
+          hotelName: booking.hotelName,
+          hotelAddress: booking.hotelAddress,
+          checkInDate: new Date(booking.checkInDate),
+          checkOutDate: new Date(booking.checkOutDate),
+          checkInTime: hotelDetails.policies.checkIn,
+          checkOutTime: hotelDetails.policies.checkOut,
+          nights: null,
+          rooms: hotelDetails.rooms
+            .filter((room) => booking.rooms.includes(room._id))
+            .map((room) => ({
+              key: room._id,
+              roomNumber: room.roomNumber,
+              floor: room.floor,
+              roomType: room.roomType,
+              bedOptions: room.bedOptions,
+              sleepsCount: room.sleepsCount,
+            })),
+          guests: booking.guests.map((guest) => ({
+            key: guest._id,
+            guestType: guest.guestType,
+            firstName: guest.firstName,
+            lastName: guest.lastName,
+            email: guest.email,
+            phone: guest.phone,
+          })),
+          totalPrice: booking.totalPrice,
+        };
+
+        data.nights = differenceInDays(data.checkOutDate, data.checkInDate);
+
+        return <HotelBookingDetailsCard key={booking._id} bookingData={data} />;
+      })}
     </div>
   );
 }
