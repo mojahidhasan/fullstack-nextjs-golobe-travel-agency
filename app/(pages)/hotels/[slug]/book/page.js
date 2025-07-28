d09dbc5539e49b878f84c89046d5c81431b2658a
mojahid <c.mojahid.me@gmail.com>
2025-07-28 16:58:53 +0600

    feat: refactor hotel booking page to validate search state and retrieve hotel details

--- app/(pages)/hotels/[slug]/book/page.js
+++ app/(pages)/hotels/[slug]/book/page.js
@@ -1,22 +1,18 @@
 import { BreadcrumbUI } from "@/components/local-ui/breadcrumb";
-import { HotelFareCard } from "@/components/FareCard";
 import { AuthenticationCard } from "@/components/AuthenticationCard";
 
 import { isLoggedIn } from "@/lib/auth";
-import { getOneDoc } from "@/lib/db/getOperationDB";
 import { notFound } from "next/navigation";
 import { cookies } from "next/headers";
 import NotFound from "@/app/not-found";
 import routes from "@/data/routes.json";
-import { HotelBookingSteps } from "./HotelBookingSteps";
+import { HotelBookingSteps } from "@/components/pages/hotels.book/HotelBookingSteps";
+import { getHotel } from "@/lib/controllers/hotels";
+import validateHotelSearchParams from "@/lib/zodSchemas/hotelSearchParams";
 
 export default async function HotelBookPage({ params }) {
   const loggedIn = await isLoggedIn();
   const slug = params.slug;
-
-  const hotelDetails = await getOneDoc("Hotel", { slug }, ["hotels"]);
-  if (!hotelDetails || Object.keys(hotelDetails).length === 0)
-    return notFound();
 
   if (!loggedIn) {
     return (
@@ -29,15 +25,21 @@ export default async function HotelBookPage({ params }) {
   const searchState = JSON.parse(
     cookies().get("hotelSearchState")?.value || "{}",
   );
-  if (Object.keys(searchState).length === 0) {
+  const validate = validateHotelSearchParams(searchState);
+
+  if (!validate.success) {
     return (
       <NotFound
-        whatHappened="search state not found"
-        explanation="Sorry, we couldn't retrieve your hotel search context. Please search again."
+        whatHappened="Error in search state"
+        explanation="Sorry, we couldn't retrieve your hotel search context or there was an error in search state. Thus we couldn't retrieve the hotel details. Please search again."
         navigateTo={{ path: routes.hotels.path, title: routes.hotels.title }}
       />
     );
   }
+
+  const hotelDetails = await getHotel(slug, searchState);
+  if (!hotelDetails || Object.keys(hotelDetails).length === 0)
+    return notFound();
 
   return (
     <>
