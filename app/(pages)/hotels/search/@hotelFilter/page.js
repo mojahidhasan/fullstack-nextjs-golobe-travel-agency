bc411da255ebbffa295f11ecb589c9c5550e97df
mojahid <c.mojahid.me@gmail.com>
2025-08-02 19:54:41 +0600

    feat: integrate hotel search filter validation and default values into HotelsFilter component
    Enhanced the FlightFilterPage by incorporating filter extraction from search parameters, validating filters, and retrieving default filter values for hotels. This update improves the HotelsFilter component's functionality by ensuring it receives validated filters and default values, enhancing the overall user experience during hotel searches.

--- app/(pages)/hotels/search/@hotelFilter/page.js
+++ app/(pages)/hotels/search/@hotelFilter/page.js
@@ -1,9 +1,17 @@
 import { HotelsFilter } from "@/components/pages/hotels.search/sections/HotelsFilter";
-function FlightFilterPage() {
+import { getHotelDefaultFilterValues } from "@/lib/controllers/hotels";
+import extractFiltersObjFromSearchParams from "@/lib/helpers/hotels/extractFiltersObjFromSearchParams";
+import validateHotelSearchFilter from "@/lib/zodSchemas/hotelSearchFilterValidation";
+async function FlightFilterPage({ searchParams }) {
+  const filtersParams = extractFiltersObjFromSearchParams(searchParams);
+  const validatedFilters = validateHotelSearchFilter(filtersParams);
+  const filterValues = await getHotelDefaultFilterValues();
+
   return (
-    <>
-      <HotelsFilter />
-    </>
+    <HotelsFilter
+      filters={validatedFilters?.data || {}}
+      defaultFiltersVals={filterValues}
+    />
   );
 }
 
