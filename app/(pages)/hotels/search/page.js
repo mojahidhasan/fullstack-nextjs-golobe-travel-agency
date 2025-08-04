375ce8278358bafe8c55978ec6141892bb05e1ed
mojahid <c.mojahid.me@gmail.com>
2025-08-04 11:31:09 +0600

    feat: update HotelSearchPage to return notFound response
    Refactored the HotelSearchPage component to return a notFound response instead of rendering the search form. This change improves error handling for invalid search parameters, enhancing the user experience by providing clearer feedback when no results are found.

--- app/(pages)/hotels/search/page.js
+++ app/(pages)/hotels/search/page.js
@@ -1,10 +1,5 @@
-import { SearchStaysForm } from "@/components/sections/SearchStaysForm";
-function HotelSearchPage({ searchParams }) {
-  return (
-    <section className="mx-auto mb-8 rounded-[16px] bg-white px-[24px] py-[32px] shadow-md">
-      <SearchStaysForm searchParams={searchParams} />
-    </section>
-  );
-}
+import { notFound } from "next/navigation";
 
-export default HotelSearchPage;
+export default function HotelSearchPage() {
+  return notFound();
+}
