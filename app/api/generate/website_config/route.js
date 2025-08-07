935a943e8ecac9e774ca42ccca215ae284fd0349
mojahid <c.mojahid.me@gmail.com>
2025-08-07 20:22:59 +0600

    feat: implement website configuration upload route
    Added a new API route for uploading website configuration to the database. This includes a POST method that checks for authorization and handles the database connection and configuration upload process. Removed the previous test file for website configuration as it is no longer needed.

---
+++ app/api/generate/website_config/route.js
@@ -0,0 +1,48 @@
+import { WebsiteConfig } from "@/admin/db/models";
+import { connectToDB } from "@/lib/db/utilsDB";
+
+const config = {
+  maintenanceMode: {
+    enabled: false,
+    message: "Site is under maintenance.",
+    startsAt: null,
+    endsAt: null,
+    type: "full",
+    affectedFeatures: [],
+    allowlistedRoutes: [
+      "/privacy-policy",
+      "/terms-of-service",
+      "/contact-us",
+      "/support",
+    ],
+    reason: "",
+  },
+  enableFlightBooking: true,
+  enableHotelBooking: true,
+};
+
+const uploadWebsiteConfigDB = async () => {
+  await connectToDB();
+  await WebsiteConfig.deleteMany({}).exec();
+  await WebsiteConfig.create(config);
+};
+
+export async function POST(req) {
+  if (
+    req.headers.get("Authorization") !==
+    `Bearer ${process.env.API_SECRET_TOKEN}`
+  ) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+  try {
+    console.log("Uploading website config DB");
+    await uploadWebsiteConfigDB();
+    console.log("Website config DB uploaded successfully");
+    return new Response("success", {
+      status: 200,
+    });
+  } catch (error) {
+    console.error("Error uploading website config DB:", error);
+    return new Response("Error uploading website config DB", { status: 500 });
+  }
+}
