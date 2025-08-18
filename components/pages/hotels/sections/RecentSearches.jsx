fe71476cd2f7846b4b0fbd7a9c425eecd76ac199
mojahid <c.mojahid.me@gmail.com>
2025-08-18 23:22:19 +0600

    feat: refactor RecentSearches component to utilize SearchHistoryCard and handle dynamic search data
    Updated the RecentSearches component to accept a dynamic array of searches, replacing static recent searches with a mapping of SearchHistoryCard components. Integrated parsing for flight and hotel search parameters to display detailed information, enhancing user experience by providing relevant search history insights.

--- components/pages/hotels/sections/RecentSearches.jsx
+++ components/pages/hotels/sections/RecentSearches.jsx
@@ -1,24 +1,55 @@
-import { RecentSearchesCard } from "@/components/pages/hotels/ui/RecentSearchesCard";
-export function RecentSearches() {
-  const recentSearches = [];
-  recentSearches.length = Math.floor(Math.random() * 2);
+import { EmptyResult } from "@/components/EmptyResult";
+import SearchHistoryCard from "@/components/SearchHistoryCard";
+import { parseFlightSearchParams } from "@/lib/utils";
+export function RecentSearches({ searchesArr = [] }) {
   return (
     <section className="mb-[80px]">
       <div className="mb-[32px] text-[2rem] font-semibold text-secondary">
         Your recent searches
       </div>
 
-      {recentSearches.length === 0 ? (
-        <div className="select-none text-center opacity-50">
-          No recent searches
-        </div>
+      {searchesArr.length === 0 ? (
+        <EmptyResult message="No recent searches" className={"w-full"} />
       ) : (
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-[16px]">
-          <RecentSearchesCard />
-          <RecentSearchesCard />
-          <RecentSearchesCard />
-          <RecentSearchesCard />
-          <RecentSearchesCard />
+        <div className="grid gap-[16px] md:grid-cols-2 lg:grid-cols-3">
+          {searchesArr.map((search) => {
+            const sState = search.searchState;
+            let data = {
+              type: search.type,
+              searchState: sState,
+              searchedAt: search.createdAt,
+            };
+
+            if (search.type === "flight") {
+              const parsedState = parseFlightSearchParams(sState);
+              data = {
+                ...data,
+                tripType: parsedState.tripType,
+                details: `${parsedState.from.city} (${parsedState.from.iataCode}) → ${parsedState.to.city} (${parsedState.to.iataCode})`,
+                class: parsedState.class,
+                passengers: Object.values(parsedState.passengers).reduce(
+                  (acc, val) => acc + val,
+                  0,
+                ),
+                departureDate: parsedState.desiredDepartureDate,
+                returnDate: parsedState.desiredReturnDate,
+              };
+            }
+
+            console.log("data", search);
+            if (search.type === "hotel") {
+              data = {
+                ...data,
+                details: `${sState.city}, ${sState.country}`,
+                rooms: sState.rooms,
+                guests: sState.guests,
+                checkInDate: +sState.checkIn,
+                checkOutDate: +sState.checkOut,
+              };
+            }
+
+            return <SearchHistoryCard key={search._id} search={data} />;
+          })}
         </div>
       )}
     </section>
